ac67744818733cb020b22bbd6391909965851d1e
PatrickFeeney <patrickfeeney1225@gmail.com>
2021-02-04 15:56:26 -0500

    Forward Test

--- tests/test_models_lsa.py
+++ tests/test_models_lsa.py
@@ -1,7 +1,20 @@
+import torch
+
 from polycraft_nov_det.models.lsa.LSA_cifar10_no_est import LSACIFAR10NoEst
+
+
+input_shape = (3, 32, 32)
+batch_size = 256
 
 
 # constructor tests
 def test_constructor_no_est():
-    LSACIFAR10NoEst((3, 32, 32), 64)
+    LSACIFAR10NoEst(input_shape, 64)
+    assert True
+
+
+# forward tests
+def test_forward_no_est():
+    model = LSACIFAR10NoEst(input_shape, 64)
+    model(torch.zeros((batch_size,) + input_shape))
     assert True
