538fce88622410d527c09ebc5b913efc202b0f70
PatrickFeeney <patrickfeeney1225@gmail.com>
2021-02-25 09:38:53 -0500

    MNIST Data and Model

--- polycraft_nov_det/data.py
+++ polycraft_nov_det/data.py
@@ -1,17 +1,33 @@
 from importlib.resources import path
 
-import torchvision.datasets
+from torch.utils import data
+from torchvision.datasets import MNIST
+from torchvision import transforms
 
 
-def torch_mnist():
-    """torchvision datasets for MNIST
+def torch_mnist(batch_size=1, shuffle=True):
+    """torch DataLoaders for MNIST
+
+    Args:
+        batch_size (int, optional): batch_size for DataLoaders. Defaults to 1.
+        shuffle (bool, optional): shuffle for DataLoaders. Defaults to True.
 
     Returns:
-        (torchvision.datasets.mnist.MNIST,
-         torchvision.datasets.mnist.MNIST): MNIST train and test sets
+        (DataLoader, DataLoader): MNIST train and test sets
     """
     # get path within module
     with path("polycraft_nov_det", "base_data") as data_path:
-        train = torchvision.datasets.MNIST(root=data_path, train=True, download=True)
-        test = torchvision.datasets.MNIST(root=data_path, train=False, download=True)
-    return train, test
+        # get dataset with separate data and targets
+        train_set = MNIST(root=data_path, train=True, download=True,
+                          target_transform=transforms.ToTensor())
+        test_set = MNIST(root=data_path, train=False, download=True,
+                         target_transform=transforms.ToTensor())
+    # adds extra channel dimension to data for compatibility with networks expecting RGB
+    train_data = train_set.data[:, None].float()
+    test_data = test_set.data[:, None].float()
+    # get tensor dataset with data and targets
+    train_tensor = data.TensorDataset(train_data, train_set.targets)
+    test_tensor = data.TensorDataset(test_data, test_set.targets)
+    # get DataLoaders for datasets
+    return (data.DataLoader(train_tensor, batch_size, shuffle),
+            data.DataLoader(test_tensor, batch_size, shuffle))
--- tests/test_models_lsa.py
+++ tests/test_models_lsa.py
@@ -1,18 +1,38 @@
 import torch
 
+from polycraft_nov_det.data import torch_mnist
 from polycraft_nov_det.models.lsa.LSA_cifar10_no_est import LSACIFAR10NoEst
+from polycraft_nov_det.models.lsa.LSA_mnist_no_est import LSAMNISTNoEst
 
 
-input_shape = (3, 32, 32)
+cifar_input_shape = (3, 32, 32)
+mnist_input_shape = (1, 28, 28)
 batch_size = 256
 
 
 # constructor tests
-def test_constructor_no_est():
-    LSACIFAR10NoEst(input_shape, 64)
+def test_constructor_cifar10_no_est():
+    LSACIFAR10NoEst(cifar_input_shape, 64)
+
+
+def test_constructor_mnist_no_est():
+    LSAMNISTNoEst(mnist_input_shape, 64)
 
 
 # forward tests
-def test_forward_no_est():
-    model = LSACIFAR10NoEst(input_shape, 64)
-    model(torch.zeros((batch_size,) + input_shape))
+def test_forward_cifar10_no_est():
+    model = LSACIFAR10NoEst(cifar_input_shape, 64)
+    model(torch.zeros((batch_size,) + cifar_input_shape))
+
+
+def test_forward_mnist_no_est():
+    model = LSAMNISTNoEst(mnist_input_shape, 64)
+    model(torch.zeros((batch_size,) + mnist_input_shape))
+
+
+def test_mnist_no_est():
+    train_loader, test_loader = torch_mnist(batch_size, False)
+    model = LSAMNISTNoEst(mnist_input_shape, 64)
+    for data, target in train_loader:
+        model(data)
+        break
