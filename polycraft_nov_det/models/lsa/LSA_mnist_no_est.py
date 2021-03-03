a4af6c47be2a3ec44ed539c7081ba4c5b095f51c
PatrickFeeney <patrickfeeney1225@gmail.com>
2021-03-03 16:08:39 -0500

    WIP Training Func

--- polycraft_nov_det/models/lsa/LSA_mnist_no_est.py
+++ polycraft_nov_det/models/lsa/LSA_mnist_no_est.py
@@ -51,3 +51,56 @@ class LSAMNISTNoEst(base.BaseModule):
         x_r = x_r.view(-1, *self.input_shape)
 
         return x_r, z
+
+
+def train():
+    from importlib.resources import path
+
+    import torch
+    import torch.nn as nn
+    import torch.optim as optim
+    from torch.utils.tensorboard import SummaryWriter
+
+    from polycraft_nov_det.data import torch_mnist
+
+    # define shape constants
+    mnist_input_shape = (1, 28, 28)
+    batch_size = 256
+    # get dataloaders
+    train_loader, valid_loader, _ = torch_mnist(batch_size, False)
+    # get Tensorboard writer
+    with path("polycraft_nov_det.models.lsa", "runs") as log_dir:
+        writer = SummaryWriter(log_dir)
+    # define training constants
+    lr = 1e-3
+    epochs = 1
+    loss_func = nn.MSELoss()
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    # construct model
+    model = LSAMNISTNoEst(mnist_input_shape, 64)
+    model.to(device)
+    # construct optimizer
+    optimizer = optim.Adam(model.parameters(), lr)
+    # train model
+    for epoch in range(epochs):
+        epoch_loss = 0
+        for data, target in train_loader:
+            data = data.to(device)
+            optimizer.zero_grad()
+            # update weights with optimizer
+            r_data, embedding = model(data)
+            batch_loss = loss_func(data, r_data)
+            batch_loss.backward()
+            optimizer.step()
+            # logging
+            epoch_loss += batch_loss.item() * batch_size
+        # record training loss
+        av_epoch_loss = epoch_loss / len(train_loader)
+        writer.add_scalar("Average Train Loss", av_epoch_loss, epoch)
+        # TODO add reconstruction visualization
+        # TODO add latent space visualization (try PCA or t-SNE for projection)
+        # TODO add validation set
+    # save model
+    with path("polycraft_nov_det/models/lsa", "LSA_mnist_no_est.pt") as f_path:
+        torch.save(model.state_dict(), f_path)
+    return model
