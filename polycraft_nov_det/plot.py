f0b999b78af2c932d1d6971d86de9ac5e9c12806
PatrickFeeney <patrickfeeney1225@gmail.com>
2021-04-06 11:30:50 -0400

    Combine Reconstruction Plot Funcs

--- polycraft_nov_det/plot.py
+++ polycraft_nov_det/plot.py
@@ -2,12 +2,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def plot_reconstruction(images, r_images):
+def plot_reconstruction(images, r_images, cmap="rgb"):
     """Plot sample of image reconstructions
 
     Args:
         images (torch.tensor): Images input to autoencoder
         r_images (torch.tensor): Reconstructed images output from autoencoder
+        cmap (str, optional): Color map for images, either "rgb" or "gray". Defaults to "rgb".
 
     Returns:
         plt.Figure: Figure with plot of sample of image reconstructions
@@ -19,45 +20,26 @@ def plot_reconstruction(images, r_images):
     # remove grad from tensors for numpy conversion
     images = images.detach().cpu()
     r_images = r_images.detach().cpu()
-    # plot the image and reconstruction side by side
+    # set up axes
     fig, ax = plt.subplots(nrows=2, ncols=num_images)
     ax[0][num_images // 2].set_title("Input")
     ax[1][num_images // 2].set_title("Reconstruction")
-    imshow_kwargs = {
-        "cmap": "gray",
-        "vmin": 0,
-        "vmax": 1,
-    }
-    for i in range(num_images):
-        ax[0][i].imshow(images[i, 0], **imshow_kwargs)
-        ax[1][i].imshow(r_images[i, 0], **imshow_kwargs)
-    # disable tick marks
-    for axis in ax.flat:
-        axis.set_xticks([])
-        axis.set_yticks([])
-    return fig
-
-
-def plot_reconstruction_rgb(images, r_images):
-    # set number of images for plot
-    num_images = 5
-    if images.shape[0] < num_images:
-        num_images = images.shape[0]
-    # remove grad from tensors for numpy conversion
-    images = images.detach().cpu()
-    r_images = r_images.detach().cpu()
-    # plot the image and reconstruction side by side
-    fig, ax = plt.subplots(nrows=2, ncols=num_images)
-    ax[0][num_images // 2].set_title("Input")
-    ax[1][num_images // 2].set_title("Reconstruction")
+    # set up imshow keyword args
     imshow_kwargs = {
         "vmin": 0,
         "vmax": 1,
     }
+    if cmap == "gray":
+        imshow_kwargs["cmap"] = cmap
+    # plot the image and reconstruction side by side
     for i in range(num_images):
-        # Transpose images in (C, H, W) format to matplotlib's (H, W, C) format
-        ax[0][i].imshow(np.transpose(images[i, :], (1, 2, 0)), **imshow_kwargs)
-        ax[1][i].imshow(np.transpose(r_images[i, :], (1, 2, 0)), **imshow_kwargs)
+        if cmap == "gray":
+            ax[0][i].imshow(images[i, 0], **imshow_kwargs)
+            ax[1][i].imshow(r_images[i, 0], **imshow_kwargs)
+        else:
+            # Transpose images in (C, H, W) format to matplotlib's (H, W, C) format
+            ax[0][i].imshow(np.transpose(images[i, :], (1, 2, 0)), **imshow_kwargs)
+            ax[1][i].imshow(np.transpose(r_images[i, :], (1, 2, 0)), **imshow_kwargs)
     # disable tick marks
     for axis in ax.flat:
         axis.set_xticks([])
