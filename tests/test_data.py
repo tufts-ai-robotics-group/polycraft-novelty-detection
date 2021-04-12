394e37444fe1912dbba0a30d9dd26caea08117ac
PatrickFeeney <patrickfeeney1225@gmail.com>
2021-04-12 15:39:13 -0400

    Test Formatting

--- tests/test_data.py
+++ tests/test_data.py
@@ -2,7 +2,10 @@ import polycraft_nov_det.data as data
 
 
 # MNIST tests
-def test_mnist():
-    train_loader, valid_loader, test_loader = data.torch_mnist(batch_size=1)
+train_loader, valid_loader, test_loader = data.torch_mnist(batch_size=1, shuffle=False)
+
+
+def test_mnist_len():
+    # check length of datasets
     assert len(train_loader) + len(valid_loader) == 60000
     assert len(test_loader) == 10000
