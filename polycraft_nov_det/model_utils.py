3281e542b60ba24ed4722144af51a4104f33df89
PatrickFeeney <patrickfeeney1225@gmail.com>
2021-05-17 15:33:00 -0400

    Generalized Model Loading

--- polycraft_nov_det/model_utils.py
+++ polycraft_nov_det/model_utils.py
@@ -2,25 +2,49 @@ import os.path
 
 import torch
 
+import polycraft_nov_data.data_const as polycraft_const
+
 import polycraft_nov_det.mnist_loader as mnist_loader
 from polycraft_nov_det.models.lsa.LSA_mnist_no_est import LSAMNISTNoEst
+from polycraft_nov_det.models.lsa.LSA_cifar10_no_est import LSACIFAR10NoEst
 from polycraft_nov_det.novelty import load_ecdf, reconstruction_ecdf
 import polycraft_nov_det.plot as plot
 
 
-def load_model(path):
-    """Load a saved model
+def load_model(path, model, device="cpu"):
+    # load parameters into a model instance
+    model.load_state_dict(torch.load(path, map_location=device))
+    return model
+
+
+def load_mnist_model(path, device="cpu", latent_len=64):
+    """Load a saved MNIST model
 
     Args:
         path (str): Path to saved model state_dict
+        device (str, optional): Device tag for torch.device. Defaults to "cpu".
+        latent_len (int, optional): Length of model's latent vector. Defaults to 64.
 
     Returns:
         LSAMNISTNoEst: Model with saved state_dict
     """
-    device = torch.device("cuda:1" if torch.cuda.is_available() else "cpu")
-    model = LSAMNISTNoEst(mnist_loader.MNIST_SHAPE, 64)
-    model.load_state_dict(torch.load(path, map_location=device))
-    return model
+    model = LSAMNISTNoEst(mnist_loader.MNIST_SHAPE, latent_len)
+    return load_model(path, model, device)
+
+
+def load_polycraft_model(path, device="cpu", latent_len=100):
+    """Load a saved Polycraft model
+
+    Args:
+        path (str): Path to saved model state_dict
+        device (str, optional): Device tag for torch.device. Defaults to "cpu".
+        latent_len (int, optional): Length of model's latent vector. Defaults to 100.
+
+    Returns:
+        LSACIFAR10NoEst: Model with saved state_dict
+    """
+    model = LSACIFAR10NoEst(polycraft_const.PATCH_SHAPE, latent_len)
+    return load_model(path, model, device)
 
 
 def load_cached_ecdfs(model_path):
