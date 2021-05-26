374ce4a55dd059d7c7bab43fbc763a9dd5911c25
PatrickFeeney <patrickfeeney1225@gmail.com>
2021-05-26 10:01:11 -0400

    Multithreading Prefetch Error Fix

--- polycraft_nov_det/mini_imagenet_loader.py
+++ polycraft_nov_det/mini_imagenet_loader.py
@@ -83,9 +83,10 @@ def mini_imagenet_dataloaders(batch_size=32, shuffle=True, all_patches=False):
     test_set = filter_dataset(dataset, include_classes=[dataset.class_to_idx["test"]])
     # get DataLoaders for datasets
     num_workers = 4
+    prefetch_factor = 1 if batch_size is None else max(batch_size//num_workers, 1)
     dataloader_kwargs = {
         "num_workers": num_workers,
-        "prefetch_factor": batch_size//num_workers,
+        "prefetch_factor": prefetch_factor,
         "persistent_workers": True,
         "batch_size": batch_size,
         "shuffle": shuffle,
