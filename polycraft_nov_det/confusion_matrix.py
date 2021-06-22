582ddd775d93bc3ea3dbee040a3fc54dba25d54b
Schneider Sarah <Sarah.Schneider@ait.ac.at>
2021-06-22 14:15:36 +0200

    Confusion matrix computation based on new dataset

--- polycraft_nov_det/confusion_matrix.py
+++ polycraft_nov_det/confusion_matrix.py
@@ -1,15 +1,17 @@
 import torch
-import torchvision
 import itertools 
 import torch.nn as nn
 import matplotlib.pyplot as plt
 import numpy as np
 from sklearn import metrics
 from sklearn.metrics import confusion_matrix
+from torch.utils import data
 
 from polycraft_nov_det.models.lsa.LSA_cifar10_no_est import LSACIFAR10NoEst 
 import polycraft_nov_data.image_transforms as image_transforms
 import polycraft_nov_data.data_const as data_const
+from polycraft_nov_data.dataloader import polycraft_dataloaders, polycraft_dataset
+import polycraft_nov_data.dataset_transforms as dataset_transforms
 
 
 # adapted from https://deeplizard.com/learn/video/0LhiS6yu2qQ 
@@ -17,6 +19,7 @@ import polycraft_nov_data.data_const as data_const
 def plot_confusion_matrix(cm, classes, t, scale, pool, cmap='BuPu'):
     
     fig3 = plt.figure()
+    print('Confusion matrix, optimal threshold is ', t)
     print(cm)
     plt.imshow(cm, interpolation='nearest', cmap=cmap)
     plt.title('Confusion matrix, scale = %s, t = %s,  pooling = %s' 
@@ -39,35 +42,54 @@ def plot_confusion_matrix(cm, classes, t, scale, pool, cmap='BuPu'):
     fig3.savefig('conf.png', bbox_inches="tight")
 
 
-def get_data_loader(image_scale, batch_size, data_dir):
-    """
-    Preprocess Images (remove minecraft bar, change image resolution,
-    extract patches of size batch_size x batch_size in ordered way with 
-    batch_size/2 overlap normalized between 0 and 1. 
-    :image_scale: scale used for decrease in resolution, 1 for original 
-    resolution
-    :param batch_size: size of the extracted patch
-    :param data_dir: root directory where the dataset is
-    :return: 2 dataloaders, one for ROC/Precision-Recall curve, one for 
-    confusion matrix
-    """
-    trnsfrm = image_transforms.TestPreprocess(image_scale)
-    
-    data = torchvision.datasets.ImageFolder(root=data_dir, transform=trnsfrm)
-    total_noi = len(data)
-    search_noi = int(0.5 * total_noi)  # Images used to find threshold, 50%
-    test_noi = total_noi - search_noi  # Images used for confusion matrix, 50%
+def polycraft_dataloaders_50_50(batch_size=32, include_classes=None, 
+                                image_scale=1.0, shuffle=True,
+                                all_patches=False):
+    """torch DataLoaders for Polycraft datasets
 
-    search_dataset, test_dataset = torch.utils.data.random_split(data,
-                                [search_noi, test_noi],
-                                generator=torch.Generator().manual_seed(42))
+    Args:
+        batch_size (int, optional): batch_size for DataLoaders. Defaults to 32.
+        include_classes (list, optional): List of names of classes to include.
+                                          Defaults to None, including all classes.
+        image_scale (float, optional): Scaling applied to images. Defaults to 1.0.
+        shuffle (bool, optional): shuffle for DataLoaders. Defaults to True.
+        all_patches (bool, optional): Whether to replace batches with all patches from an image.
+                                      Defaults to False.
 
-    search_loader = torch.utils.data.DataLoader(search_dataset, batch_size, 
-                                                shuffle=False)
-    test_loader = torch.utils.data.DataLoader(test_dataset, batch_size, 
-                                               shuffle=False)
-    
-    return search_loader, test_loader
+    Returns:
+        (DataLoader, DataLoader): Polycraft dataloader with novelty.
+                                              Contains batches of (3, 32, 32) images,
+                                              with values 0-1.
+    """
+    # if using patches, override batch dim to hold the set of patches
+    if not all_patches:
+        collate_fn = None
+        transform = image_transforms.TrainPreprocess(image_scale)
+    else:
+        batch_size = None
+        collate_fn = dataset_transforms.collate_patches
+        transform = image_transforms.TestPreprocess(image_scale)
+    # get the dataset
+    dataset = polycraft_dataset(transform)
+    # update include_classes to use indices instead of names
+    include_classes = dataset_transforms.folder_name_to_target(dataset, include_classes)
+    # split into datasets
+    search_set, test_set = dataset_transforms.filter_split(
+        dataset, [.5, .5], include_classes
+    )
+    # get DataLoaders for datasets
+    num_workers = 4
+    prefetch_factor = 1 if batch_size is None else max(batch_size//num_workers, 1)
+    dataloader_kwargs = {
+        "num_workers": num_workers,
+        "prefetch_factor": prefetch_factor,
+        "persistent_workers": True,
+        "batch_size": batch_size,
+        "shuffle": shuffle,
+        "collate_fn": collate_fn,
+    }
+    return (data.DataLoader(search_set, **dataloader_kwargs),
+            data.DataLoader(test_set, **dataloader_kwargs))
 
 
 def plot_roc(p, n, fp, tp, scale, pool):
@@ -128,10 +150,9 @@ def compute_tp_tn_fp_fn(normaldata, noveldata, model, allts, device, pooling,
     """
     N = len(normaldata)  # all images are non-novel
     P = len(noveldata)  # all images are novel
-    maxiter = np.minimum(N, P) 
-    N = maxiter
-    P = maxiter
-
+    print('Number of non-novel images used to determine threshold: ', N)
+    print('Number of novel images used to determine threshold: ', P)
+    
     loss_func2d = nn.MSELoss(reduction='none')
     
     FP = np.zeros(len(allts))
@@ -141,15 +162,11 @@ def compute_tp_tn_fp_fn(normaldata, noveldata, model, allts, device, pooling,
     
     with torch.no_grad():
 
-        print('Iterate through non-novel images.')
         for i, sample in enumerate(normaldata):
-            if i == maxiter:
-                break
-            
+           
             patches = sample[0]
-            flat_patches = torch.flatten(patches, start_dim=1, end_dim=2)
-    
-            x = flat_patches[0].float().to(device)
+            
+            x = patches.float().to(device)
             x.requires_grad = False
             x_rec, z = model(x)
     
@@ -169,22 +186,17 @@ def compute_tp_tn_fp_fn(normaldata, noveldata, model, allts, device, pooling,
                 if pooled_loss > t:
                     novelty_score = True
                     
-                if novelty_score == True:
+                if novelty_score is True:
                     FP[ii] += 1
                 
-                if novelty_score == False:
+                if novelty_score is False:
                     TN[ii] += 1
 
-        print('Iterate through novel images.')
         for i, sample in enumerate(noveldata):
 
-            if i == maxiter:
-                break
-
             patches = sample[0]
-            flat_patches = torch.flatten(patches, start_dim=1, end_dim=2)
-    
-            x = flat_patches[0].float().to(device)
+            
+            x = patches.float().to(device)
             x.requires_grad = False
             x_rec, z = model(x)
     
@@ -201,10 +213,10 @@ def compute_tp_tn_fp_fn(normaldata, noveldata, model, allts, device, pooling,
                 if pooled_loss > t:
                     novelty_score = True
                     
-                if novelty_score == True:
+                if novelty_score is True:
                     TP[ii] += 1
                 
-                if novelty_score == False:
+                if novelty_score is False:
                     FN[ii] += 1  
                     
     plot_roc(P, N, FP, TP, scale, pooling)
@@ -224,8 +236,7 @@ def find_optimal_treshold(P, N, FP, TP, FN, TN, allts):
     total_cost = cost_FP * frac_FP + cost_TP * frac_TP + cost_TN * frac_TN + cost_FN * frac_FN
     optimal_idx = np.argmin(total_cost)
     optimal_threshold = allts[optimal_idx]
-    print("Optimal threshold value is:", optimal_threshold)
-    
+   
     return optimal_threshold
 
 def compute_confusion_matrix(t_opt, normal_test, novel_test, model, device, 
@@ -238,10 +249,10 @@ def compute_confusion_matrix(t_opt, normal_test, novel_test, model, device,
     loss_func2d = nn.MSELoss(reduction='none')
     N = len(normal_test)  # all images are non-novel
     P = len(novel_test) # all images are novel
-    maxiter = np.minimum(N, P)
-    N = maxiter
-    P = maxiter
-    
+   
+    print('Number of non-novel images used for confusion matrix: ', N)
+    print('Number of novel images used for confusion matrix: ', P)
+   
     label_nono = np.zeros((N), dtype=bool)  # Label all normal images as False
     label_no = np.ones((P), dtype=bool)  # Label all normal images as True
     labels = np.append(label_nono, label_no)
@@ -249,16 +260,11 @@ def compute_confusion_matrix(t_opt, normal_test, novel_test, model, device,
     
     with torch.no_grad():
     
-        print('Iterate through non-novel images.')
         for i, sample in enumerate(normal_test):
 
-            if i == maxiter:
-                break
-
             patches = sample[0]
-            flat_patches = torch.flatten(patches, start_dim=1, end_dim=2)
-    
-            x = flat_patches[0].float().to(device)
+            
+            x = patches.float().to(device)
             x.requires_grad = False
             x_rec, z = model(x)
     
@@ -277,16 +283,11 @@ def compute_confusion_matrix(t_opt, normal_test, novel_test, model, device,
             
             pred.append(novelty_score)
 
-        print('Iterate through novel images.')
         for i, sample in enumerate(novel_test):
 
-            if i == maxiter:
-                break
-
             patches = sample[0]
-            flat_patches = torch.flatten(patches, start_dim=1, end_dim=2)
-    
-            x = flat_patches[0].float().to(device)
+            
+            x = patches.float().to(device)
             x.requires_grad = False
             x_rec, z = model(x)
     
@@ -310,15 +311,34 @@ def compute_confusion_matrix(t_opt, normal_test, novel_test, model, device,
     return  cm
     
 
-def performance_evaluation(model_directory, scale, n_z):
+def performance_evaluation():
+    """
+    Computes ROC and precision-recall curve for one (unseen) subset of non-novel
+    and novel images. Then these can be used to compute an "optimal" threshold,
+    which we evaluate on the other (unseen) subset of non-novel and novel 
+    images.
+    """
     
-    data_directory_nono = 'unseen_data/no_novelties'
-    data_directory_no = 'unseen_data/item_novelty'
-
+    model_directory = './models/polycraft/noisy/scale_1/8000.pt'
+    scale = 1
+    n_z = 100
     pool = 'max'
+    
     b_size = 1
     pc_input_shape = data_const.PATCH_SHAPE  # color channels, height, width
+    
     device = torch.device("cuda:1" if torch.cuda.is_available() else "cpu")
+    
+    # Let's use valid. set for threshold selection, test set for conf. matrix
+    _, normal_search, normal_test = polycraft_dataloaders(batch_size=1, 
+                                    include_classes=['normal'], 
+                                    image_scale=scale, all_patches=True)
+    
+    # Split novelty images in two sets and use one for threshold selection,
+    # the other for conf. matrix
+    novel_search, novel_test = polycraft_dataloaders_50_50(batch_size=1, 
+                               include_classes=['item'], 
+                               image_scale=scale, all_patches=True)
     
     # construct model
     model = LSACIFAR10NoEst(pc_input_shape, n_z)
@@ -348,25 +368,19 @@ def performance_evaluation(model_directory, scale, n_z):
         allts2 = np.round(np.linspace(0.04, 0.07, 20), 4)
         allts = np.append(allts1, allts2)
     
-    normal_search, normal_test = get_data_loader(scale, b_size, 
-                                                 data_dir=data_directory_nono)
-    novel_search, novel_test = get_data_loader(scale, b_size, 
-                                               data_dir=data_directory_no)
-    
     p, n, fp, tp, fn, tn = compute_tp_tn_fp_fn(normal_search, novel_search, 
                                                model, allts, device, pool, 
                                                scale)
     
     thresh = find_optimal_treshold(p, n, fp, tp, fn, tn, allts)
-    
     cm = compute_confusion_matrix(thresh, normal_test, novel_test, model,
                                   device, pool)
-    classes = (['not novel', 'novel'])
     
-    plot_confusion_matrix(cm, classes, thresh, scale, pool )
+    classes = (['not novel', 'novel'])
+    plot_confusion_matrix(cm, classes, thresh, scale, pool)
     
 
 if __name__ == '__main__':
-    model_dir = 'models/polycraft/noisy/scale_0_75/8000.pt'
-    performance_evaluation(model_dir, 0.75, 100)
+    
+    performance_evaluation()
     
