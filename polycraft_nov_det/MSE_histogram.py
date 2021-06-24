e86c64e5a700b4fc580295f4b3004ac5b9ea1836
PatrickFeeney <patrickfeeney1225@gmail.com>
2021-06-24 10:00:56 -0400

    Code Formatting

--- polycraft_nov_det/MSE_histogram.py
+++ polycraft_nov_det/MSE_histogram.py
@@ -34,8 +34,8 @@ def roi_coords_from_csv(csv_file):
 class ROIPatchSampler(Dataset):
     """
     This patch sampler extracts a pair of image patches, one patch is
-    extracted randomly from a region with a novelty, the other from a non-novel 
-    region. 
+    extracted randomly from a region with a novelty, the other from a non-novel
+    region.
     The novel region is specified in a csv file by the x- and y-coordinates
     of the upper left corner and its width and height.
     """
@@ -83,13 +83,14 @@ class ROIPatchSampler(Dataset):
         X_min = sec_marg
         X_max = W - sec_marg
         Y_min = sec_marg
-        Y_max = H - sec_marg 
+        Y_max = H - sec_marg
         dx = self.patch_size
         dy = self.patch_size
 
         max_n = 1000
-        offset1 = 4 * ((4 - self.size + 1)/2) # just to make sure that the novelty is more centered
-        offset2 = 4 * ((4 - self.size + 1)/2)  # just to make sure that we do not capture a novelty here
+        offset1 = 4 * ((4 - self.size + 1)/2)  # just to make sure that the novelty is more centered
+        # just to make sure that we do not capture a novelty here
+        offset2 = 4 * ((4 - self.size + 1)/2)
 
         # -- novelty patch ----------------------------------------------------
         X, Y = 0, 0
@@ -97,8 +98,8 @@ class ROIPatchSampler(Dataset):
 
         while(X < X_min or X > X_max or Y < Y_min or Y > Y_max):
             # coordinates of center pixel, offset1 to get a more central region
-            Y = rng.randint(y_idx + offset1, y_idx + height - offset1) 
-            X = rng.randint(x_idx + offset1, x_idx + width - offset1) 
+            Y = rng.randint(y_idx + offset1, y_idx + height - offset1)
+            X = rng.randint(x_idx + offset1, x_idx + width - offset1)
             n += 1
 
             if (n > max_n):
@@ -110,7 +111,8 @@ class ROIPatchSampler(Dataset):
 
         # -- no-novelty patch ------------------------------------------------
         n = 0
-        while((X > x_idx-offset2 and X < (x_idx + width + offset2)) or (Y > y_idx-offset2 and Y < (y_idx + height+offset2))):
+        while ((X > x_idx-offset2 and X < (x_idx + width + offset2)) or
+               (Y > y_idx-offset2 and Y < (y_idx + height+offset2))):
             X = rng.randint(X_min, X_max)
             Y = rng.randint(Y_min, Y_max)
             n += 1
@@ -118,7 +120,7 @@ class ROIPatchSampler(Dataset):
                 print('No non-novel region where a whole patch fits in!')
                 break
 
-        no_novelty_patch  = image[Y-int(dy/2):Y+int(dy/2),X-int(dx/2):X+int(dx/2),:]
+        no_novelty_patch = image[Y-int(dy/2):Y+int(dy/2), X-int(dx/2):X+int(dx/2), :]
 
         return novelty_patch, no_novelty_patch
 
@@ -129,17 +131,17 @@ class ROIPatchSampler(Dataset):
 
     def get_roi_coordinates(self):
         rois = roi_coords_from_csv(self.root_dir + '/size' + str(self.size) + '.csv')
-        return rois    
+        return rois
 
 
 def plot_loss_his(scale, size, model_path, epochs):
-    """Plots a histogram of reconstruction losses of novel and non-novel 
-    patches. The plotted loss values are computed based on the MSE loss 
-    averaged over a patch. 
+    """Plots a histogram of reconstruction losses of novel and non-novel
+    patches. The plotted loss values are computed based on the MSE loss
+    averaged over a patch.
     Args:
         scale: Resolution scaling factor (1 --> original resolution)
         size: Size of the novelty, 0 for largest, 4 for smalles size of novelty
-        root_dir: Directory of the images and csv file 
+        root_dir: Directory of the images and csv file
         epochs: Number of randomly selected novel/non-novel patch-pairs per
                 image
     """
@@ -149,10 +151,9 @@ def plot_loss_his(scale, size, model_path, epochs):
     kwargs = {'num_workers': 1, 'pin_memory': True} if device == 'cuda' else {}
     pc_input_shape = data_const.PATCH_SHAPE
     n_z = 100
-    root_dir='datasets/novelty_evaluation_size/' + str(size) + '_/' + str(size)
+    root_dir = 'datasets/novelty_evaluation_size/' + str(size) + '_/' + str(size)
 
     trnfrm = transforms.Compose([transforms.ToTensor(), image_transforms.CropUI()])
-    
 
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     # construct model
@@ -166,43 +167,44 @@ def plot_loss_his(scale, size, model_path, epochs):
     all_loss_nono = []
 
     for epoch in range(epochs):
-        data = ROIPatchSampler(epoch, scale=scale, size=size, root_dir=root_dir, 
-                               transform=trnfrm)     
+        data = ROIPatchSampler(epoch, scale=scale, size=size, root_dir=root_dir,
+                               transform=trnfrm)
         train_loader = DataLoader(data, batch_size=1, shuffle=False, **kwargs)
-        
+
         for i, sample in enumerate(train_loader):
 
             patch_no = sample[0]  # patch with novelty
             patch_nono = sample[1]  # patch without any novelty
 
-            #plt.imshow(np.transpose(patch_nono[0, :,:,:].detach().numpy(), (1, 2, 0)))
-            #plt.show()
-            #plt.imshow(np.transpose(patch_no[0, :,:,:].detach().numpy(), (1, 2, 0)))
-            #plt.show()
+            # plt.imshow(np.transpose(patch_nono[0, :,:,:].detach().numpy(), (1, 2, 0)))
+            # plt.show()
+            # plt.imshow(np.transpose(patch_no[0, :,:,:].detach().numpy(), (1, 2, 0)))
+            # plt.show()
 
-            x_nono = patch_nono 
+            x_nono = patch_nono
             x_rec_nono, z = model(x_nono.float().to(device))
-            x_no = patch_no 
+            x_no = patch_no
             x_rec_no, z = model(x_no.float().to(device))
 
-            #MSE loss, averaged over patch
+            # MSE loss, averaged over patch
             loss_nono = loss_func(x_nono, x_rec_nono)
             loss_no = loss_func(x_no, x_rec_no)
             all_loss_nono.append(loss_nono.item())
             all_loss_no.append(loss_no.item())
-            
-            #if loss_no.item() > 0.01:
+
+            # if loss_no.item() > 0.01:
             #    plt.imshow(np.transpose(patch_no[0, :,:,:].detach().numpy(), (1, 2, 0)))
             #    plt.show()
 
-            #x_rec_nono = x_rec_nono.detach().numpy()
-            #plt.imshow(np.transpose(x_rec_nono[0], (1, 2, 0)))
-            #plt.show()
-            #x_rec_no = x_rec_no.detach().numpy()
-            #plt.imshow(np.transpose(x_rec_no[0], (1, 2, 0)))
-            #plt.show()
+            # x_rec_nono = x_rec_nono.detach().numpy()
+            # plt.imshow(np.transpose(x_rec_nono[0], (1, 2, 0)))
+            # plt.show()
+            # x_rec_no = x_rec_no.detach().numpy()
+            # plt.imshow(np.transpose(x_rec_no[0], (1, 2, 0)))
+            # plt.show()
 
-    plt.hist([all_loss_nono, all_loss_no], bins=140, alpha=0.7, color=("b", "m"), label=['no novelty', 'novelty'])
+    plt.hist([all_loss_nono, all_loss_no], bins=140, alpha=0.7,
+             color=("b", "m"), label=['no novelty', 'novelty'])
     plt.legend()
     plt.title('Patch-wise MSE loss, size %s, scale %s' % (str(size), str(scale)))
     plt.yticks([])
@@ -213,9 +215,9 @@ def plot_loss_his(scale, size, model_path, epochs):
 
 if __name__ == '__main__':
 
-    scale = 0.75   
+    scale = 0.75
     size = 1
     noe = 20  # Number of randomly selected patch pairs per image
     model_path = 'models/polycraft/no_noise/scale_0_75/7200.pt'
-    
+
     plot_loss_his(scale, size, model_path, epochs=noe)
--- polycraft_nov_det/confusion_matrix.py
+++ polycraft_nov_det/confusion_matrix.py
@@ -1,5 +1,5 @@
 import torch
-import itertools 
+import itertools
 import torch.nn as nn
 import matplotlib.pyplot as plt
 import numpy as np
@@ -7,22 +7,22 @@ from sklearn import metrics
 from sklearn.metrics import confusion_matrix
 from torch.utils import data
 
-from polycraft_nov_det.models.lsa.LSA_cifar10_no_est import LSACIFAR10NoEst 
+from polycraft_nov_det.models.lsa.LSA_cifar10_no_est import LSACIFAR10NoEst
 import polycraft_nov_data.image_transforms as image_transforms
 import polycraft_nov_data.data_const as data_const
 from polycraft_nov_data.dataloader import polycraft_dataloaders, polycraft_dataset
 import polycraft_nov_data.dataset_transforms as dataset_transforms
 
 
-# adapted from https://deeplizard.com/learn/video/0LhiS6yu2qQ 
+# adapted from https://deeplizard.com/learn/video/0LhiS6yu2qQ
 # - CNN Confusion Matrix with PyTorch - Neural Network Programming
 def plot_confusion_matrix(cm, classes, t, scale, pool, cmap='BuPu'):
-    
+
     fig3 = plt.figure()
     print('Confusion matrix, optimal threshold is ', t)
     print(cm)
     plt.imshow(cm, interpolation='nearest', cmap=cmap)
-    plt.title('Confusion matrix, scale = %s, t = %s,  pooling = %s' 
+    plt.title('Confusion matrix, scale = %s, t = %s,  pooling = %s'
               % (str(scale), str(t), pool))
     plt.colorbar()
     tick_marks = np.arange(len(classes))
@@ -31,9 +31,9 @@ def plot_confusion_matrix(cm, classes, t, scale, pool, cmap='BuPu'):
 
     fmt = 'd'
     color_thresh = cm.max() / 2.
-    
+
     for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
-        plt.text(j, i, format(cm[i, j], fmt), horizontalalignment="center", 
+        plt.text(j, i, format(cm[i, j], fmt), horizontalalignment="center",
                  color="white" if cm[i, j] > color_thresh else "black")
 
     plt.tight_layout()
@@ -42,7 +42,7 @@ def plot_confusion_matrix(cm, classes, t, scale, pool, cmap='BuPu'):
     fig3.savefig('conf.png', bbox_inches="tight")
 
 
-def polycraft_dataloaders_50_50(batch_size=32, include_classes=None, 
+def polycraft_dataloaders_50_50(batch_size=32, include_classes=None,
                                 image_scale=1.0, shuffle=True,
                                 all_patches=False):
     """torch DataLoaders for Polycraft datasets
@@ -130,19 +130,19 @@ def plot_precision_recall(tp, tn, fp, fn, scale, pool):
     auc_pr = metrics.auc(recall, prec)
 
     plt.figure()
-    plt.plot(recall, prec, linestyle='--', marker='o', color='m', lw=2, 
-                                                                 clip_on=False)
+    plt.plot(recall, prec, linestyle='--', marker='o', color='m', lw=2,
+             clip_on=False)
     plt.plot([0, 1], [0.5, 0.5], color='navy', linestyle='--')
     plt.xlim([0.0, 1.0])
     plt.ylim([0.0, 1.0])
     plt.xlabel('Recall')
     plt.ylabel('Precision')
-    plt.title('AUC = %.2f, scale = %s, pooling = %s' % 
-                                                    (auc_pr, str(scale), pool))
+    plt.title('AUC = %.2f, scale = %s, pooling = %s' %
+              (auc_pr, str(scale), pool))
     plt.savefig('PR.png')
-   
-    
-def compute_tp_tn_fp_fn(normaldata, noveldata, model, allts, device, pooling, 
+
+
+def compute_tp_tn_fp_fn(normaldata, noveldata, model, allts, device, pooling,
                         scale):
     """
     Compute true positives, true negatives, false positives and false negatives
@@ -152,82 +152,82 @@ def compute_tp_tn_fp_fn(normaldata, noveldata, model, allts, device, pooling,
     P = len(noveldata)  # all images are novel
     print('Number of non-novel images used to determine threshold: ', N)
     print('Number of novel images used to determine threshold: ', P)
-    
+
     loss_func2d = nn.MSELoss(reduction='none')
-    
+
     FP = np.zeros(len(allts))
     TP = np.zeros(len(allts))
     FN = np.zeros(len(allts))
     TN = np.zeros(len(allts))
-    
+
     with torch.no_grad():
 
         for i, sample in enumerate(normaldata):
-           
+
             patches = sample[0]
-            
+
             x = patches.float().to(device)
             x.requires_grad = False
             x_rec, z = model(x)
-    
+
             loss2d = loss_func2d(x_rec, x)
             loss2d = torch.mean(loss2d, (1, 2, 3))  # averaged loss per patch
-            
+
             if pooling == 'mean':
                 # mean of all patch losses
-                pooled_loss = torch.mean(loss2d).item()  
+                pooled_loss = torch.mean(loss2d).item()
             if pooling == 'max':
                 # maximum of all patch losses
-                pooled_loss = torch.max(loss2d).item()  
-            
+                pooled_loss = torch.max(loss2d).item()
+
             for ii, t in enumerate(allts):
                 novelty_score = False
-                
+
                 if pooled_loss > t:
                     novelty_score = True
-                    
+
                 if novelty_score is True:
                     FP[ii] += 1
-                
+
                 if novelty_score is False:
                     TN[ii] += 1
 
         for i, sample in enumerate(noveldata):
 
             patches = sample[0]
-            
+
             x = patches.float().to(device)
             x.requires_grad = False
             x_rec, z = model(x)
-    
+
             loss2d = loss_func2d(x_rec, x)
             loss2d = torch.mean(loss2d, (1, 2, 3))  # averaged loss per patch
-            
+
             if pooling == 'mean':
-                pooled_loss = torch.mean(loss2d).item()  
+                pooled_loss = torch.mean(loss2d).item()
             if pooling == 'max':
-                pooled_loss = torch.max(loss2d).item()  
-                
+                pooled_loss = torch.max(loss2d).item()
+
             for ii, t in enumerate(allts):
                 novelty_score = False
                 if pooled_loss > t:
                     novelty_score = True
-                    
+
                 if novelty_score is True:
                     TP[ii] += 1
-                
+
                 if novelty_score is False:
-                    FN[ii] += 1  
-                    
+                    FN[ii] += 1
+
     plot_roc(P, N, FP, TP, scale, pooling)
     plot_precision_recall(TP, TN, FP, FN, scale, pooling)
 
-    return  P, N, FP, TP, FN, TN
+    return P, N, FP, TP, FN, TN
 
 
 def find_optimal_treshold(P, N, FP, TP, FN, TN, allts):
     """
-    Compute optimal treshold based on true positives, true negatives, false 
+    Compute optimal treshold based on true positives, true negatives, false
     positives and false negatives using cost function. The cost of false
     positives and false negatives are set to 1 (for now).
     """
@@ -236,121 +236,121 @@ def find_optimal_treshold(P, N, FP, TP, FN, TN, allts):
     total_cost = cost_FP * frac_FP + cost_TP * frac_TP + cost_TN * frac_TN + cost_FN * frac_FN
     optimal_idx = np.argmin(total_cost)
     optimal_threshold = allts[optimal_idx]
-   
+
     return optimal_threshold
 
-def compute_confusion_matrix(t_opt, normal_test, novel_test, model, device, 
+
+def compute_confusion_matrix(t_opt, normal_test, novel_test, model, device,
                              pooling):
     """
-    Compute confusion matrix for the second half of the available unseen data 
-    by using the optimal threshold we determined for the first half of the 
+    Compute confusion matrix for the second half of the available unseen data
+    by using the optimal threshold we determined for the first half of the
     unseen data.
     """
     loss_func2d = nn.MSELoss(reduction='none')
     N = len(normal_test)  # all images are non-novel
-    P = len(novel_test) # all images are novel
-   
+    P = len(novel_test)  # all images are novel
+
     print('Number of non-novel images used for confusion matrix: ', N)
     print('Number of novel images used for confusion matrix: ', P)
-   
+
     label_nono = np.zeros((N), dtype=bool)  # Label all normal images as False
     label_no = np.ones((P), dtype=bool)  # Label all normal images as True
     labels = np.append(label_nono, label_no)
     pred = []
-    
+
     with torch.no_grad():
-    
+
         for i, sample in enumerate(normal_test):
 
             patches = sample[0]
-            
+
             x = patches.float().to(device)
             x.requires_grad = False
             x_rec, z = model(x)
-    
+
             loss2d = loss_func2d(x_rec, x)
-            loss2d = torch.mean(loss2d, (1, 2, 3)) # averaged loss per patch
-            
+            loss2d = torch.mean(loss2d, (1, 2, 3))  # averaged loss per patch
+
             if pooling == 'mean':
-                pooled_loss = torch.mean(loss2d).item()  
+                pooled_loss = torch.mean(loss2d).item()
             if pooling == 'max':
-                pooled_loss = torch.max(loss2d).item()  
-            
+                pooled_loss = torch.max(loss2d).item()
+
             novelty_score = False
-               
+
             if pooled_loss > t_opt:
                 novelty_score = True
-            
+
             pred.append(novelty_score)
 
         for i, sample in enumerate(novel_test):
 
             patches = sample[0]
-            
+
             x = patches.float().to(device)
             x.requires_grad = False
             x_rec, z = model(x)
-    
+
             loss2d = loss_func2d(x_rec, x)
             loss2d = torch.mean(loss2d, (1, 2, 3))  # averaged loss per patch
-            
+
             if pooling == 'mean':
                 pooled_loss = torch.mean(loss2d).item()
             if pooling == 'max':
                 pooled_loss = torch.max(loss2d).item()
-              
+
             novelty_score = False
-             
+
             if pooled_loss > t_opt:
                 novelty_score = True
-            
+
             pred.append(novelty_score)
-            
+
     cm = confusion_matrix(labels, pred)  # Determine confusion matrix
 
-    return  cm
-    
+    return cm
+
 
 def performance_evaluation():
     """
     Computes ROC and precision-recall curve for one (unseen) subset of non-novel
     and novel images. Then these can be used to compute an "optimal" threshold,
-    which we evaluate on the other (unseen) subset of non-novel and novel 
+    which we evaluate on the other (unseen) subset of non-novel and novel
     images.
     """
-    
+
     model_directory = './models/polycraft/noisy/scale_1/8000.pt'
     scale = 1
     n_z = 100
     pool = 'max'
-    
-    b_size = 1
+
     pc_input_shape = data_const.PATCH_SHAPE  # color channels, height, width
-    
+
     device = torch.device("cuda:1" if torch.cuda.is_available() else "cpu")
-    
+
     # Let's use valid. set for threshold selection, test set for conf. matrix
-    _, normal_search, normal_test = polycraft_dataloaders(batch_size=1, 
-                                    include_classes=['normal'], 
-                                    image_scale=scale, all_patches=True)
-    
+    _, normal_search, normal_test = polycraft_dataloaders(batch_size=1,
+                                                          include_classes=['normal'],
+                                                          image_scale=scale, all_patches=True)
+
     # Split novelty images in two sets and use one for threshold selection,
     # the other for conf. matrix
-    novel_search, novel_test = polycraft_dataloaders_50_50(batch_size=1, 
-                               include_classes=['item'], 
-                               image_scale=scale, all_patches=True)
-    
+    novel_search, novel_test = polycraft_dataloaders_50_50(batch_size=1,
+                                                           include_classes=['item'],
+                                                           image_scale=scale, all_patches=True)
+
     # construct model
     model = LSACIFAR10NoEst(pc_input_shape, n_z)
-    model.load_state_dict(torch.load(model_directory, 
+    model.load_state_dict(torch.load(model_directory,
                                      map_location=torch.device('cpu')))
     model.eval()
     model.to(device)
-    
+
     if scale == 0.75 or scale == 0.5:
-    
+
         #  max, 0.75, 0.5
-        allts = np.round(np.linspace(0.003, 0.013, 30), 4) 
+        allts = np.round(np.linspace(0.003, 0.013, 30), 4)
         allts = np.append(0.000005, allts)
         allts = np.append(0.0005, allts)
         allts = np.append(allts, 0.015)
@@ -360,27 +360,26 @@ def performance_evaluation():
         allts = np.append(allts, 0.04)
         allts = np.append(allts, 0.05)
         allts = np.append(allts, 0.06)
-    
-    if scale == 1: 
-    
+
+    if scale == 1:
+
         #  max, 1
-        allts1 = np.round(np.linspace(0.003, 0.04, 40), 4) 
+        allts1 = np.round(np.linspace(0.003, 0.04, 40), 4)
         allts2 = np.round(np.linspace(0.04, 0.07, 20), 4)
         allts = np.append(allts1, allts2)
-    
-    p, n, fp, tp, fn, tn = compute_tp_tn_fp_fn(normal_search, novel_search, 
-                                               model, allts, device, pool, 
+
+    p, n, fp, tp, fn, tn = compute_tp_tn_fp_fn(normal_search, novel_search,
+                                               model, allts, device, pool,
                                                scale)
-    
+
     thresh = find_optimal_treshold(p, n, fp, tp, fn, tn, allts)
     cm = compute_confusion_matrix(thresh, normal_test, novel_test, model,
                                   device, pool)
-    
+
     classes = (['not novel', 'novel'])
     plot_confusion_matrix(cm, classes, thresh, scale, pool)
-    
+
 
 if __name__ == '__main__':
-    
+
     performance_evaluation()
-    
