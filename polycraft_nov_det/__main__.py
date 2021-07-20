70861857884dab3b55efddef417c46dfc2d416d5
PatrickFeeney <patrickfeeney1225@gmail.com>
2021-07-20 13:47:51 -0400

    Remove Mini-ImageNet

--- polycraft_nov_det/__main__.py
+++ polycraft_nov_det/__main__.py
@@ -3,7 +3,6 @@ import argparse
 from polycraft_nov_data.data_const import PATCH_SHAPE
 from polycraft_nov_data.dataloader import polycraft_dataloaders
 
-import polycraft_nov_det.mini_imagenet_loader as mini_imagenet_loader
 import polycraft_nov_det.mnist_loader as mnist_loader
 import polycraft_nov_det.models.lsa.LSA_cifar10_no_est as LSA_cifar10_no_est
 import polycraft_nov_det.models.lsa.LSA_mnist_no_est as LSA_mnist_no_est
@@ -12,7 +11,7 @@ import polycraft_nov_det.train as train
 
 # construct argument parser
 parser = argparse.ArgumentParser(description="Polycraft Novelty Detection Model Training")
-parser.add_argument("model", choices=["mnist", "polycraft", "miniimagenet"],
+parser.add_argument("model", choices=["mnist", "polycraft"],
                     help="Model to train")
 # Polycraft specific args
 polycraft_group = parser.add_argument_group("Polycraft")
@@ -68,20 +67,6 @@ elif args.model == "polycraft":
     # get model instance
     latent_len = 100 if args.latent_len is None else args.latent_len
     model = LSA_cifar10_no_est.LSACIFAR10NoEst(PATCH_SHAPE, latent_len)
-# handle Mini-ImageNet args
-elif args.model == "miniimagenet":
-    # set default train kwargs
-    train_kwargs = {
-        "lr": 1e-3,
-        "epochs": 1000,
-        "gpu": 1,
-    }
-    # get dataloaders
-    batch_size = 128 if args.batch_size is None else args.batch_size
-    train_loader, valid_loader, _ = mini_imagenet_loader.mini_imagenet_dataloaders(batch_size)
-    # get model instance
-    latent_len = 100 if args.latent_len is None else args.latent_len
-    model = LSA_cifar10_no_est.LSACIFAR10NoEst(mini_imagenet_loader.PATCH_SHAPE, latent_len)
 
 
 # update train_kwargs with parsed args
