cdd8218c31cd32bf89c424b64e2c1023861bb28a
Sarah Schneider <schneiders@C1SHP9901L>
2021-08-24 14:16:59 +0200

    Acc. for better training overview, add squared differnces

--- polycraft_nov_det/eval/binary_classification_training_positions.py
+++ polycraft_nov_det/eval/binary_classification_training_positions.py
@@ -5,10 +5,11 @@ import torch.nn as nn
 import torch.optim as optim
 from torch.utils.tensorboard import SummaryWriter
 import numpy as np
+from torch.utils.data import Dataset, DataLoader
 
 from polycraft_nov_data import data_const as polycraft_const
 import polycraft_nov_det.model_utils as model_utils
-from polycraft_nov_data.dataloader import polycraft_dataloaders, polycraft_dataset
+from polycraft_nov_data.dataloader import polycraft_dataset_for_ms, polycraft_dataset
 
 
 class binaryClassification(nn.Module):
@@ -46,12 +47,28 @@ def normalize_index_range(index_range):
     return index_range_norm
 
 
-def train_on_loss_vector(model_paths):
-    lr = 0.003
-    epochs = 300
+class TrippleDataset(Dataset):
+    def __init__(self, datasetA, datasetB, datasetC):
+        self.datasetA = datasetA
+        self.datasetB = datasetB
+        self.datasetC = datasetC
+        
+    def __getitem__(self, index):
+        xA = self.datasetA[index]
+        xB = self.datasetB[index]
+        xC = self.datasetC[index]
+        return xA, xB, xC
+    
+    def __len__(self):
+        return len(self.datasetA)
+
+
+def train_on_loss_vector_pos(model_paths):
+    lr = 0.001
+    epochs = 1000
     b_s = 10
     
-    device = torch.device("cuda:1" if torch.cuda.is_available() else "cpu")
+    device = torch.device("cuda:2" if torch.cuda.is_available() else "cpu")
     classifier = binaryClassification()
     classifier.to(device)
     optimizer = optim.Adam(classifier.parameters(), lr)
@@ -68,18 +85,24 @@ def train_on_loss_vector(model_paths):
     model_path1 = Path(model_paths[2])
     model1 = model_utils.load_polycraft_model(model_path1, device).eval()
 
-    _, valid_loader05, test_loader05 = polycraft_dataloaders(batch_size=b_s,
+    _, valid_set05, test_set05 = polycraft_dataset_for_ms(batch_size=1,
                                                             image_scale=0.5, 
                                                             include_novel=True, 
-                                                            shuffle=True)
-    _, valid_loader075, test_loader075 = polycraft_dataloaders(batch_size=b_s,
+                                                            shuffle=False)
+    _, valid_set075, test_set075 = polycraft_dataset_for_ms(batch_size=1,
                                                             image_scale=0.75, 
                                                             include_novel=True, 
-                                                            shuffle=True)
-    _, valid_loader1, test_loader1 = polycraft_dataloaders(batch_size=b_s,
+                                                            shuffle=False)
+    _, valid_set1, test_set1 = polycraft_dataset_for_ms(batch_size=1,
                                                             image_scale=1, 
                                                             include_novel=True, 
-                                                            shuffle=True)
+                                                            shuffle=False)
+    
+    valid_set = TrippleDataset(valid_set05, valid_set075, valid_set1)
+    valid_loader = DataLoader(valid_set, batch_size=1, shuffle=True)
+    
+    test_set = TrippleDataset(test_set05, test_set075, test_set1)
+    test_loader = DataLoader(test_set, batch_size=1, shuffle=True)
     
     # get targets determined at runtime
     base_dataset = polycraft_dataset()                                         
@@ -111,9 +134,10 @@ def train_on_loss_vector(model_paths):
         print('Epoch number  ', epoch, flush=True)
         train_loss = 0
         valid_loss = 0
+        train_acc = 0
+        valid_acc = 0
     
-        for i, samples in enumerate(zip(valid_loader05, valid_loader075, 
-                                        valid_loader1)):
+        for i, samples in enumerate(valid_loader):
             
             feature_vector = []
             
@@ -121,7 +145,8 @@ def train_on_loss_vector(model_paths):
                 
                 for n, model in enumerate(all_models):
                         
-                    patches = samples[n][0]
+                    patches = samples[n][0][0]
+                    patches = torch.flatten(patches, start_dim=0, end_dim=1)
                     ipt_shape = ipt_shapes[n]    
                     _, ih, iw = polycraft_const.IMAGE_SHAPE
                     _, ph, pw = polycraft_const.PATCH_SHAPE
@@ -139,12 +164,12 @@ def train_on_loss_vector(model_paths):
                     j_idx_norm.append(j_ranges_norm[n][max_idx[1]])
                     
                     feature_vector.append(maxloss)
-                feature_vector.append(abs(i_idx_norm[0] - i_idx_norm[1]))
-                feature_vector.append(abs(i_idx_norm[0] - i_idx_norm[2]))
-                feature_vector.append(abs(i_idx_norm[1] - i_idx_norm[2]))
-                feature_vector.append(abs(j_idx_norm[0] - j_idx_norm[1]))
-                feature_vector.append(abs(j_idx_norm[0] - j_idx_norm[2]))
-                feature_vector.append(abs(j_idx_norm[1] - j_idx_norm[2]))
+                feature_vector.append((i_idx_norm[0] - i_idx_norm[1])**2)
+                feature_vector.append((i_idx_norm[0] - i_idx_norm[2])**2)
+                feature_vector.append((i_idx_norm[1] - i_idx_norm[2])**2)
+                feature_vector.append((j_idx_norm[0] - j_idx_norm[1])**2)
+                feature_vector.append((j_idx_norm[0] - j_idx_norm[2])**2)
+                feature_vector.append((j_idx_norm[1] - j_idx_norm[2])**2)
                     
             label = samples[0][1]  
                 
@@ -158,16 +183,23 @@ def train_on_loss_vector(model_paths):
             loss = BCEloss(pred, target)
             loss.backward()
             optimizer.step()
+            
+            if pred >= 0.5 and target.to(device) == torch.ones(1).to(device):
+                train_acc += 1
+            if pred < 0.5 and target.to(device) == torch.zeros(1).to(device):
+                train_acc += 1
          
             # logging
-            train_loss += loss.item()*b_s
+            train_loss += loss.item()
             
-        train_loss = train_loss / (len(valid_loader05))
+        train_loss = train_loss / (len(valid_loader))
+        train_acc = train_acc / (len(valid_loader))
         writer.add_scalar("Average Train Loss", train_loss, epoch)
         print('Average training loss  ', train_loss, flush=True)
+        writer.add_scalar("Average Train Acc", train_acc, epoch)
+        print('Average training Acc  ', train_acc, flush=True)
            
-        for i, samples in enumerate(zip(test_loader05, test_loader075, 
-                                        test_loader1)):
+        for i, samples in enumerate(test_loader):
             
             feature_vector = []
             
@@ -175,7 +207,8 @@ def train_on_loss_vector(model_paths):
                 
                 for n, model in enumerate(all_models):
                         
-                    patches = samples[n][0]
+                    patches = samples[n][0][0]
+                    patches = torch.flatten(patches, start_dim=0, end_dim=1)
                     ipt_shape = ipt_shapes[n]     
                     _, ih, iw = polycraft_const.IMAGE_SHAPE
                     _, ph, pw = polycraft_const.PATCH_SHAPE
@@ -193,12 +226,12 @@ def train_on_loss_vector(model_paths):
                     j_idx_norm.append(j_ranges_norm[n][max_idx[1]])
                     
                     feature_vector.append(maxloss)
-                feature_vector.append(abs(i_idx_norm[0] - i_idx_norm[1]))
-                feature_vector.append(abs(i_idx_norm[0] - i_idx_norm[2]))
-                feature_vector.append(abs(i_idx_norm[1] - i_idx_norm[2]))
-                feature_vector.append(abs(j_idx_norm[0] - j_idx_norm[1]))
-                feature_vector.append(abs(j_idx_norm[0] - j_idx_norm[2]))
-                feature_vector.append(abs(j_idx_norm[1] - j_idx_norm[2])) 
+                feature_vector.append((i_idx_norm[0] - i_idx_norm[1])**2)
+                feature_vector.append((i_idx_norm[0] - i_idx_norm[2])**2)
+                feature_vector.append((i_idx_norm[1] - i_idx_norm[2])**2)
+                feature_vector.append((j_idx_norm[0] - j_idx_norm[1])**2)
+                feature_vector.append((j_idx_norm[0] - j_idx_norm[2])**2)
+                feature_vector.append((j_idx_norm[1] - j_idx_norm[2])**2) 
                     
             label = samples[0][1]  
                 
@@ -210,12 +243,20 @@ def train_on_loss_vector(model_paths):
             pred = classifier(torch.FloatTensor(feature_vector).to(device))
             loss = BCEloss(pred, target)
             
+            if pred >= 0.5 and target.to(device) == torch.ones(1).to(device):
+                valid_acc += 1
+            if pred < 0.5 and target.to(device) == torch.zeros(1).to(device):
+                valid_acc += 1
+              
             # logging
-            valid_loss += loss.item()*b_s 
+            valid_loss += loss.item()
         
-        valid_loss = valid_loss / (len(test_loader05))
+        valid_loss = valid_loss / (len(test_loader))
+        valid_acc = valid_acc / (len(test_loader))
         writer.add_scalar("Average Validation Loss", valid_loss, epoch)
+        writer.add_scalar("Average Validation Acc", valid_acc, epoch)
         print('Average Validation loss  ', valid_loss, flush=True)
+        print('Average Validation Acc  ', valid_acc, flush=True)
         
         # save model
         if (epoch + 1) % (epochs // 10) == 0 or epoch == epochs - 1:
@@ -230,6 +271,4 @@ if __name__ == '__main__':
     path075 = 'models/polycraft/no_noise/scale_0_75/8000.pt'
     path1 = 'models/polycraft/no_noise/scale_1/8000.pt'
     paths = [path05, path075, path1]
-    train_on_loss_vector(paths)
-
-   
+    train_on_loss_vector_pos(paths)
