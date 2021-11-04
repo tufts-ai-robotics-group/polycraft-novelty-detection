d05c1dd8a36f954356f44af42c73c2a0b9cdbb05
Sarah Schneider <schneiders@C1SHP9901L>
2021-11-04 14:22:56 +0100

    optional use of model trained on 16x16 patches at scale 1

--- polycraft_nov_det/eval/eval.py
+++ polycraft_nov_det/eval/eval.py
@@ -11,6 +11,7 @@ from polycraft_nov_data import data_const as polycraft_const
 from polycraft_nov_data.dataloader import polycraft_dataloaders, polycraft_dataset
 from polycraft_nov_data.dataset_transforms import folder_name_to_target_list
 from polycraft_nov_data.data_const import PATCH_SHAPE
+from polycraft_nov_det.models.lsa.LSA_cifar10_no_est import LSACIFAR10NoEst
 
 import polycraft_nov_data.dataloader as dataloader
 import polycraft_nov_det.eval.plot as eval_plot
@@ -67,31 +68,47 @@ def eval_base(model_path, model, dataloaders, normal_targets, novel_targets, poo
     eval_plot.plot_con_matrix(con_matrix).savefig(eval_path / Path("con_matrix.png"))
     
     
-def eval_polycraft_multiscale(model_paths, device="cpu"):
-    
+def eval_polycraft_multiscale(model_paths, device="cpu", add_16x16_model=False):
+    """
+    Evaluate the binary classifier model trained on the three-dimensional rec.-error array 
+    of each scale. The rec.-errors of the individual colour channels are not
+    averaged for this model. At scale 0.5 and scale 0.75 we have one model each with
+    patches of size 3x32x32, at scale 1 we have one model based on patch size
+    3x32x32 and, optionally, an additional model based on patch size 3x16x16.
+    Set add_16x16 to True if you want to use thi additonal fourth model.
+    """
+
     # model_paths:
         # model_paths[0] --> scale 0.5 autoencoder model
         # model_paths[1] --> scale 0.75 autoencoder model
         # model_paths[2] --> scale 1 autoencoder model
         # model_paths[3] --> binary classifier trained on rgb loss arrays
-        
-    eval_path = Path(Path(model_paths[3]).parent, "eval_" + Path(model_paths[3]).stem)
+
+    eval_path = Path(Path(model_paths[0]).parent, "eval_" + Path(model_paths[0]).stem)
     eval_path.mkdir(exist_ok=True)
-    
-    # Use the model trained on rec.-loss error   
-    bc_path = model_paths[3]
-    classifier = ms_classifier.MultiscaleClassifierConvFeatComp(3, 429)
-    classifier = model_utils.load_model(bc_path, classifier, device).eval()
+
+    # Use the model trained on rec.-loss error
+    bc_path = model_paths[0]
+
+    # If we use additional model trained on 3x16x16 patches at scale 1 as well
+    if add_16x16_model:
+        classifier = ms_classifier.MultiscaleClassifierConvFeatComp4Models(3, 572)
+        classifier = model_utils.load_model(bc_path, classifier, device).eval()
+    # Otherwise just use models trained on 3x32x32 patches at each scale  
+    else:
+        classifier = ms_classifier.MultiscaleClassifierConvFeatComp3Models(3, 429)
+        classifier = model_utils.load_model(bc_path, classifier, device).eval()
+
     classifier.to(device)
-    
+
     # Use 3x32x32 patch based model on all three scales (for now)
-    model_path05 = Path(model_paths[0])
+    model_path05 = Path(model_paths[1])
     model05 = model_utils.load_polycraft_model(model_path05, device).eval()
-    model_path075 = Path(model_paths[1])
+    model_path075 = Path(model_paths[2])
     model075 = model_utils.load_polycraft_model(model_path075, device).eval()
-    model_path1 = Path(model_paths[2])
+    model_path1 = Path(model_paths[3])
     model1 = model_utils.load_polycraft_model(model_path1, device).eval()
-    
+
     _, _, test_set05 = dataloader.polycraft_dataset_for_ms(batch_size=1,
                                                             image_scale=0.5, 
                                                             patch_shape=PATCH_SHAPE,
@@ -107,7 +124,8 @@ def eval_polycraft_multiscale(model_paths, device="cpu"):
                                                             patch_shape=PATCH_SHAPE,
                                                             include_novel=True, 
                                                             shuffle=False)
-    
+
+    # Construct a dataset with the same images at each scale
     test_set = dataloader.TrippleDataset(test_set05, test_set075, test_set1)
     test_loader = DataLoader(test_set, batch_size=1, shuffle=False)
 
@@ -116,18 +134,47 @@ def eval_polycraft_multiscale(model_paths, device="cpu"):
     rec_loss2d = nn.MSELoss(reduction='none')
     all_models = [model05.to(device), model075.to(device), model1.to(device)]
 
-    pos, neg = 0, 0
+    # shapes of "patch array" for all scales.
+    ipt_shapes = [[6, 7],  # Scale 0.5, patch size 3x32x32
+                  [9, 11],  # Scale 0.75, patch size 3x32x32
+                  [13, 15]]  # Scale 1, patch size 3x32x32
+
+    # If we use additional model trained on 3x16x16 patches at scale 1 
+    if add_16x16_model:
+
+        # we need this additonal model
+        model_path1_16 = Path(model_paths[4])
+        model1_16 = LSACIFAR10NoEst((3, 16, 16), 25)
+        model1_16.load_state_dict(torch.load(model_path1_16, map_location=device))
+        model1_16.eval()
+
+        # and patches of scale 1 of size 3x16x16
+        _, _, test_set1_16 = dataloader.polycraft_dataset_for_ms(batch_size=1,
+                                                            image_scale=1, 
+                                                            patch_shape=(3, 16, 16),
+                                                            include_novel=True, 
+                                                            shuffle=False)
+        # Construct a dataset with the same images at each scale
+        test_set = dataloader.QuattroDataset(test_set05, test_set075, 
+                                             test_set1, test_set1_16)
+        test_loader = DataLoader(test_set, batch_size=1, shuffle=False)
+        
+        all_models = [model05.to(device), model075.to(device), 
+                      model1.to(device), model1_16.to(device)]
+        
+        # shapes of "patch array" for all scales + one for the 16x16 model
+        ipt_shapes = [[6, 7],  # Scale 0.5, patch size 3x32x32
+                      [9, 11],  # Scale 0.75, patch size 3x32x32
+                      [13, 15],  # Scale 1, patch size 3x32x32
+                      [28, 31]]  # Scale 1, patch size 3x16x316
+        
+    pos, neg = 0, 0  # total number of novel (pos) and non-novel (neg) images
     thresholds = np.round(np.linspace(0.01, 1, 21), 4)
     alltps = np.zeros(len(thresholds))
     allfps = np.zeros(len(thresholds))
     alltns = np.zeros(len(thresholds))
     allfns = np.zeros(len(thresholds))
 
-    # shapes of "patch array" for all scales.
-    ipt_shapes = [[6, 7],
-                  [9, 11],
-                  [13, 15]]
-    
     with torch.no_grad():
         for i, samples in enumerate(test_loader):
 
@@ -160,14 +207,14 @@ def eval_polycraft_multiscale(model_paths, device="cpu"):
 
             label = samples[0][1]
 
-            if label == 0 or label == 1:
+            if label == 0 or label == 1:  # Item and height novelty
                 target = True
                 pos += 1
-            if label == 2:
+            if label == 2:  # No novolty
                 target = False
                 neg += 1
 
-            # Classifier was trained over array of losses
+            # Classifier was trained over array of RGB losses
             pred = classifier(loss_arrays)
 
             for ii, t in enumerate(thresholds):
@@ -181,24 +228,31 @@ def eval_polycraft_multiscale(model_paths, device="cpu"):
                 if pred < t and target is True:
                     allfns[ii] += 1
                     
-    con_matrix = eval_stats.optimal_con_matrix(alltps, allfps, alltns, allfns)        
+    con_matrix = eval_stats.optimal_con_matrix(alltps, allfps, alltns, allfns)
     opt_index = eval_stats.optimal_index(alltps, allfps, alltns, allfns)
     opt_thresh = thresholds[opt_index]
-   
-    eval_plot.plot_con_matrix(con_matrix).savefig(eval_path / Path("con_matrix.png"))
-     
-    del base_dataset
-    
 
-"""    
+    eval_plot.plot_con_matrix(con_matrix).savefig(eval_path / Path("con_matrix.png"))
+
+    del base_dataset
+
+
 if __name__ == '__main__':
-    
+
+    dev = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+    path_classifier = 'models/polycraft/binary_classification/threshold_selection_conv_v3_rgb_500.pt'
     path05 = 'models/polycraft/no_noise/scale_0_5/8000.pt'
     path075 = 'models/polycraft/no_noise/scale_0_75/8000.pt'
     path1 = 'models/polycraft/no_noise/scale_1/8000.pt'
-    path_classifier = 'models/polycraft/binary_classification/threshold_selection_conv_v3_rgb_500.pt'
-    paths = [path05, path075, path1, path_classifier]
-    dev = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-    eval_polycraft_multiscale(paths, device=dev)
-"""    
+
+    #paths = [path_classifier, path05, path075, path1]
+    #eval_polycraft_multiscale(paths, device=dev)
+
+    path_classifier_16 =  'models/polycraft/binary_classification/threshold_selection_conv_v3_rgb_16_720.pt'
+    path1_16 = 'models/polycraft/no_noise/scale_1_16/8000.pt'
+    paths = [path_classifier_16, path05, path075, path1, path1_16]
+    eval_polycraft_multiscale(paths, device=dev, add_16x16_model=True)
+    
+  
    
--- polycraft_nov_det/models/multiscale_classifier.py
+++ polycraft_nov_det/models/multiscale_classifier.py
@@ -3,6 +3,10 @@ import torch.nn as nn
 
 
 class MultiscaleClassifierConv(nn.Module):
+    """
+    Binary classifier model trained on the one-dimensional (avgd. over all 
+    colour channels) rec.-error array of each scale
+    """
     def __init__(self, conv_output_len):
         super(MultiscaleClassifierConv, self).__init__()
         self.conv_0_5 = nn.Conv2d(1, 1, 3)
@@ -31,6 +35,12 @@ class MultiscaleClassifierConv(nn.Module):
 
 
 class MultiscaleClassifierFeatureVector(nn.Module):
+    """
+    Binary classifier model trained on a feature vectors. The feature vectors
+    are either composed of the maximum rec.-error at each scale or the 
+    maximum rec.- error (at each scale) and the distances between their 
+    "patch-positions" (between each scale).
+    """
     def __init__(self, ipt_channel):
         super(MultiscaleClassifierFeatureVector, self).__init__()
         self.ll1 = nn.Linear(ipt_channel, 12)
@@ -49,10 +59,15 @@ class MultiscaleClassifierFeatureVector(nn.Module):
         return output
 
 
-#  More comparable to feature vecture approach
-class MultiscaleClassifierConvFeatComp(nn.Module):
+class MultiscaleClassifierConvFeatComp3Models(nn.Module):
+    """
+    Binary classifier model trained on the three-dimensional rec.-error array 
+    of each scale computed based on autoencoder models with patches 3x32x32. 
+    The rec.-errors of the individual colour channels are not
+    averaged for this model. 
+    """
     def __init__(self, nc, conv_output_len):
-        super(MultiscaleClassifierConvFeatComp, self).__init__()
+        super(MultiscaleClassifierConvFeatComp3Models, self).__init__()
         self.conv_0_5 = nn.Conv2d(nc, 1, 3)
         self.conv_0_75 = nn.Conv2d(nc, 1, 3)
         self.conv_1 = nn.Conv2d(nc, 1, 3)
@@ -74,6 +89,52 @@ class MultiscaleClassifierConvFeatComp(nn.Module):
         out_0_75 = torch.flatten(self.conv_0_75(input_0_75), start_dim=1)
         out_1 = torch.flatten(self.conv_1(input_1), start_dim=1)
         out = self.relu(torch.cat((out_0_5, out_0_75, out_1), dim=1))
+
+        out = self.relu(self.fc1(out))
+        out = self.relu(self.fc2(out))
+        out = self.relu(self.fc_out(out))
+
+        out = self.relu(self.ll1(out))
+        out = self.relu(self.ll2(out))
+        output = self.sigmoid(self.llout(out))
+
+        return output
+    
+    
+class MultiscaleClassifierConvFeatComp4Models(nn.Module):
+    """
+    Binary classifier model trained on the three-dimensional rec.-error array 
+    of each scale. At scale 0.5 and scale 0.75 we have one model each with
+    patches of size 3x32x32, at scale 1 we have one model based on patch size
+    3x32x32 and an additional model based on patch size 3x16x16.
+    The rec.-errors of the individual colour channels are not
+    averaged for this model. 
+    """
+    def __init__(self, nc, conv_output_len):
+        super(MultiscaleClassifierConvFeatComp4Models, self).__init__()
+        self.conv_0_5 = nn.Conv2d(nc, 1, 3)
+        self.conv_0_75 = nn.Conv2d(nc, 1, 3)
+        self.conv_1 = nn.Conv2d(nc, 1, 3)
+        self.conv_1b = nn.Conv2d(nc, 1, 3)
+
+        self.fc1 = nn.Linear(conv_output_len, 256)
+        self.fc2 = nn.Linear(256, 256)
+        self.fc_out = nn.Linear(256, 9)
+
+        self.ll1 = nn.Linear(9, 12)
+        self.ll2 = nn.Linear(12, 12)
+        self.llout = nn.Linear(12, 1)
+
+        self.relu = nn.ReLU()
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, inputs):
+        input_0_5, input_0_75, input_1, input_1b = inputs
+        out_0_5 = torch.flatten(self.conv_0_5(input_0_5), start_dim=1)
+        out_0_75 = torch.flatten(self.conv_0_75(input_0_75), start_dim=1)
+        out_1 = torch.flatten(self.conv_1(input_1), start_dim=1)
+        out_1b = torch.flatten(self.conv_1b(input_1b), start_dim=1)
+        out = self.relu(torch.cat((out_0_5, out_0_75, out_1, out_1b), dim=1))
 
         out = self.relu(self.fc1(out))
         out = self.relu(self.fc2(out))
