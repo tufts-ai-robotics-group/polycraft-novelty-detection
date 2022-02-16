db0dc5c619e4851891c00c7d34528dd59930b5c0
PatrickFeeney <patrickfeeney1225@gmail.com>
2022-02-16 10:53:50 -0500

    Update CIFAR Len Test

--- tests/test_cifar_loader.py
+++ tests/test_cifar_loader.py
@@ -6,12 +6,14 @@ def test_cifar10_len():
     norm_targets, novel_targets, dataloaders = cifar_loader.torch_cifar(
         range(5), batch_size=1, shuffle=False)
     train_loader, valid_loader, test_loader = dataloaders
-    assert len(train_loader) + len(valid_loader) == 25000
+    assert valid_loader is None
+    assert len(train_loader) == 25000
     assert len(test_loader) == 5000
     norm_targets, novel_targets, dataloaders = cifar_loader.torch_cifar(
         range(5), batch_size=1, include_novel=True, shuffle=False)
     train_loader, valid_loader, test_loader = dataloaders
-    assert len(train_loader) + len(valid_loader) == 50000
+    assert valid_loader is None
+    assert len(train_loader) == 50000
     assert len(test_loader) == 10000
 
 
@@ -20,10 +22,12 @@ def test_cifar100_len():
     norm_targets, novel_targets, dataloaders = cifar_loader.torch_cifar(
         range(50), batch_size=1, shuffle=False, use_10=False)
     train_loader, valid_loader, test_loader = dataloaders
-    assert len(train_loader) + len(valid_loader) == 25000
+    assert valid_loader is None
+    assert len(train_loader) == 25000
     assert len(test_loader) == 5000
     norm_targets, novel_targets, dataloaders = cifar_loader.torch_cifar(
         range(50), batch_size=1, include_novel=True, shuffle=False, use_10=False)
     train_loader, valid_loader, test_loader = dataloaders
-    assert len(train_loader) + len(valid_loader) == 50000
+    assert valid_loader is None
+    assert len(train_loader) == 50000
     assert len(test_loader) == 10000
