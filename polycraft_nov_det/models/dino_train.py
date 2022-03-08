4ffde60bd9bf03dcdfc35cf5be6f685e611a5bcd
PatrickFeeney <patrickfeeney1225@gmail.com>
2022-03-08 09:38:49 -0500

    DINO Head

---
+++ polycraft_nov_det/models/dino_train.py
@@ -0,0 +1,23 @@
+import torch.nn as nn
+
+from polycraft_nov_det.models.dino.vision_transformer import DINOHead, VisionTransformer
+
+
+class DinoWithHead(nn.Module):
+    def __init__(self, dino_backbone: VisionTransformer):
+        super().__init__()
+        self.backbone = dino_backbone
+        self.head = DINOHead()
+        self.freeze_upper_blocks()
+
+    def freeze_upper_blocks(self):
+        # freeze all but last block of backbone and head
+        for name, param in self.backbone.named_parameters():
+            if "blocks.11" in name:
+                break
+            param.requires_grad = False
+
+    def forward(self, x):
+        out = self.backbone(x)
+        out = self.head(x)
+        return out
--- tests/test_dino.py
+++ tests/test_dino.py
@@ -2,8 +2,8 @@ import pytest
 import torch
 
 import polycraft_nov_det.data.cifar_loader as cifar_loader
+from polycraft_nov_det.models.dino_train import DinoWithHead
 from polycraft_nov_det.model_utils import load_dino_pretrained
-from polycraft_nov_det.models.dino.vision_transformer import DINOHead
 
 
 batch_size = 64
@@ -12,7 +12,7 @@ batch_size = 64
 class TestCIFAR10():
     @pytest.fixture
     def model(self):
-        return load_dino_pretrained()
+        return DinoWithHead(load_dino_pretrained())
 
     def test_forward(self, model):
         model(torch.zeros((batch_size,) + cifar_loader.CIFAR_SHAPE))
@@ -21,8 +21,6 @@ class TestCIFAR10():
         norm_targets, novel_targets, dataloaders = cifar_loader.torch_cifar(
             range(5), batch_size=batch_size, shuffle=False)
         train_loader, valid_loader, test_loader = dataloaders
-        head = DINOHead()
         for data, targets in train_loader:
             out = model(data)
-            out = head(out)
             break
