9a1b8630151ead531f77feda76203de3d49a67e5
PatrickFeeney <patrickfeeney1225@gmail.com>
2022-03-23 10:18:10 -0400

    Semi-Supervised K-Means++

--- polycraft_nov_det/ss_kmeans.py
+++ polycraft_nov_det/ss_kmeans.py
@@ -1,22 +1,26 @@
 import numpy as np
 import scipy.sparse as sp
-from sklearn.metrics.pairwise import _euclidean_distances
+from sklearn.metrics.pairwise import euclidean_distances
 from sklearn.utils import check_array, check_random_state
 from sklearn.utils.extmath import stable_cumsum, row_norms
 
 
 # modified version of SKLearn algorithms
 def ss_kmeans_plusplus(
-    X, n_clusters, *, x_squared_norms=None, random_state=None, n_local_trials=None
+    X, y, X_unlabeled, n_clusters_unlabled, *, x_squared_norms=None, random_state=None,
+    n_local_trials=None
 ):
-    """Init n_clusters seeds according to k-means++
-    .. versionadded:: 0.24
+    """Init n_clusters seeds according to semi-supervised k-means++
     Parameters
     ----------
     X : {array-like, sparse matrix} of shape (n_samples, n_features)
-        The data to pick seeds from.
-    n_clusters : int
-        The number of centroids to initialize
+        The labeled data to pick seeds from.
+    y : {array-like, sparse matrix} of shape (n_samples,)
+        The labels for X.
+    X_unlabeled : {array-like, sparse matrix} of shape (n_samples, n_features)
+        The unlabeled data to pick seeds from.
+    n_clusters_unlabled : int
+        The number of centroids to initialize from unlabeled data.
     x_squared_norms : array-like of shape (n_samples,), default=None
         Squared Euclidean norm of each data point.
     random_state : int or RandomState instance, default=None
@@ -39,21 +43,22 @@ def ss_kmeans_plusplus(
     # Check data
     check_array(X, accept_sparse="csr", dtype=[np.float64, np.float32])
 
-    if X.shape[0] < n_clusters:
+    n_unlabeled = X_unlabeled.shape[0]
+    if n_unlabeled < n_clusters_unlabled:
         raise ValueError(
-            f"n_samples={X.shape[0]} should be >= n_clusters={n_clusters}."
+            f"n_samples={n_unlabeled} should be >= n_clusters_unlabled={n_clusters_unlabled}."
         )
 
     # Check parameters
     if x_squared_norms is None:
-        x_squared_norms = row_norms(X, squared=True)
+        x_squared_norms = row_norms(X_unlabeled, squared=True)
     else:
         x_squared_norms = check_array(x_squared_norms, dtype=X.dtype, ensure_2d=False)
 
-    if x_squared_norms.shape[0] != X.shape[0]:
+    if x_squared_norms.shape[0] != X_unlabeled.shape[0]:
         raise ValueError(
             f"The length of x_squared_norms {x_squared_norms.shape[0]} should "
-            f"be equal to the length of n_samples {X.shape[0]}."
+            f"be equal to the length of n_samples {n_unlabeled}."
         )
 
     if n_local_trials is not None and n_local_trials < 1:
@@ -66,23 +71,29 @@ def ss_kmeans_plusplus(
 
     # Call private semi-supervised k-means++
     centers, indices = _ss_kmeans_plusplus(
-        X, n_clusters, x_squared_norms, random_state, n_local_trials
+        X, y, X_unlabeled, n_clusters_unlabled, x_squared_norms, random_state, n_local_trials
     )
 
     return centers, indices
 
 
-def _ss_kmeans_plusplus(X, n_clusters, x_squared_norms, random_state, n_local_trials=None):
-    """Computational component for initialization of n_clusters by
+def _ss_kmeans_plusplus(
+    X, y, X_unlabeled, n_clusters_unlabled, x_squared_norms, random_state, n_local_trials=None
+):
+    """Computational component for initialization of n_clusters by semi-supervised
     k-means++. Prior validation of data is assumed.
     Parameters
     ----------
-    X : {ndarray, sparse matrix} of shape (n_samples, n_features)
-        The data to pick seeds for.
-    n_clusters : int
-        The number of seeds to choose.
+    X : {array-like, sparse matrix} of shape (n_samples, n_features)
+        The labeled data to pick seeds from.
+    y : {array-like, sparse matrix} of shape (n_samples,)
+        The labels for X.
+    X_unlabeled : {array-like, sparse matrix} of shape (n_samples, n_features)
+        The unlabeled data to pick seeds from.
+    n_clusters_unlabled : int
+        The number of centroids to initialize from unlabeled data.
     x_squared_norms : ndarray of shape (n_samples,)
-        Squared Euclidean norm of each data point.
+        Squared Euclidean norm of each data point in X_unlabeled.
     random_state : RandomState instance
         The generator used to initialize the centers.
         See :term:`Glossary <random_state>`.
@@ -95,38 +106,32 @@ def _ss_kmeans_plusplus(X, n_clusters, x_squared_norms, random_state, n_local_tr
     -------
     centers : ndarray of shape (n_clusters, n_features)
         The initial centers for k-means.
-    indices : ndarray of shape (n_clusters,)
-        The index location of the chosen centers in the data array X. For a
-        given index and center, X[index] = center.
     """
-    n_samples, n_features = X.shape
+    _, n_features = X.shape
 
-    centers = np.empty((n_clusters, n_features), dtype=X.dtype)
+    centers_unlabeled = np.empty((n_clusters_unlabled, n_features), dtype=X.dtype)
 
     # Set the number of local seeding trials if none is given
     if n_local_trials is None:
         # This is what Arthur/Vassilvitskii tried, but did not report
         # specific results for other than mentioning in the conclusion
         # that it helped.
-        n_local_trials = 2 + int(np.log(n_clusters))
+        n_local_trials = 2 + int(np.log(n_clusters_unlabled))
 
-    # Pick first center randomly and track index of point
-    center_id = random_state.randint(n_samples)
-    indices = np.full(n_clusters, -1, dtype=int)
-    if sp.issparse(X):
-        centers[0] = X[center_id].toarray()
-    else:
-        centers[0] = X[center_id]
-    indices[0] = center_id
+    # Calculate centers for labeled points
+    targets = np.unique(y)
+    centers_labeled = np.empty((len(targets), n_features), dtype=X.dtype)
+    for i, target in enumerate(targets):
+        centers_labeled[i] = np.mean(X[y == target], axis=0)
 
     # Initialize list of closest distances and calculate current potential
-    closest_dist_sq = _euclidean_distances(
-        centers[0, np.newaxis], X, Y_norm_squared=x_squared_norms, squared=True
+    closest_dist_sq = euclidean_distances(
+        centers_labeled, X_unlabeled, Y_norm_squared=x_squared_norms, squared=True
     )
     current_pot = closest_dist_sq.sum()
 
-    # Pick the remaining n_clusters-1 points
-    for c in range(1, n_clusters):
+    # Pick the remaining n_clusters_unlabled points
+    for c in range(n_clusters_unlabled):
         # Choose center candidates by sampling with probability proportional
         # to the squared distance to the closest existing center
         rand_vals = random_state.random_sample(n_local_trials) * current_pot
@@ -135,8 +140,8 @@ def _ss_kmeans_plusplus(X, n_clusters, x_squared_norms, random_state, n_local_tr
         np.clip(candidate_ids, None, closest_dist_sq.size - 1, out=candidate_ids)
 
         # Compute distances to center candidates
-        distance_to_candidates = _euclidean_distances(
-            X[candidate_ids], X, Y_norm_squared=x_squared_norms, squared=True
+        distance_to_candidates = euclidean_distances(
+            X_unlabeled[candidate_ids], X_unlabeled, Y_norm_squared=x_squared_norms, squared=True
         )
 
         # update closest distances squared and potential for each candidate
@@ -150,10 +155,9 @@ def _ss_kmeans_plusplus(X, n_clusters, x_squared_norms, random_state, n_local_tr
         best_candidate = candidate_ids[best_candidate]
 
         # Permanently add best center candidate found in local tries
-        if sp.issparse(X):
-            centers[c] = X[best_candidate].toarray()
+        if sp.issparse(X_unlabeled):
+            centers_unlabeled[c] = X_unlabeled[best_candidate].toarray()
         else:
-            centers[c] = X[best_candidate]
-        indices[c] = best_candidate
+            centers_unlabeled[c] = X_unlabeled[best_candidate]
 
-    return centers, indices
+    return np.vstack((centers_labeled, centers_unlabeled))
