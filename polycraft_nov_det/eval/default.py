7b5dd9d59b7ea9a57256211056296e40d3e74b85
PatrickFeeney <patrickfeeney1225@gmail.com>
2022-03-31 11:13:03 -0400

    GCD Evals

--- polycraft_nov_det/eval/default.py
+++ polycraft_nov_det/eval/default.py
@@ -1,5 +1,6 @@
 import polycraft_nov_det.eval.evals as evals
-from polycraft_nov_det.model_load import load_autonovel_resnet, load_autonovel_pretrained
+from polycraft_nov_det.model_load import load_autonovel_resnet, load_autonovel_pretrained, \
+    load_dino_block, load_dino_pretrained
 
 
 # pretrained models
@@ -20,6 +21,10 @@ def cifar10_autonovel_pretrained():
         load_autonovel_pretrained("models/AutoNovel/discovery/resnet_IL_cifar10.pth", 10, 5))
 
 
+def cifar10_gcd_pretrained():
+    evals.cifar10_gcd(load_dino_pretrained())
+
+
 # default trained models
 
 
@@ -36,3 +41,7 @@ def cifar10_supervised():
 def cifar10_autonovel():
     evals.cifar10_autonovel(
         load_autonovel_resnet("models/CIFAR10/discovery/200.pt", 10, 5))
+
+
+def cifar10_gcd():
+    evals.cifar10_gcd(load_dino_block("models/CIFAR10/GCD/block200.pt"))
--- polycraft_nov_det/eval/evals.py
+++ polycraft_nov_det/eval/evals.py
@@ -2,6 +2,7 @@ import numpy as np
 
 from polycraft_nov_det.data.cifar_loader import torch_cifar
 import polycraft_nov_det.eval.stats as stats
+from polycraft_nov_det.ss_kmeans import SSKMeans
 
 
 def cifar10_self_supervised(model, device="cpu"):
@@ -60,6 +61,31 @@ def cifar10_autonovel(model, device="cpu"):
         norm_mask = targets < len(norm_targets)
         y_true = np.hstack((y_true, targets.cpu().numpy()[~norm_mask.cpu()] - len(norm_targets)))
         y_pred = np.hstack((y_pred, unlabel_pred_max[~norm_mask.cpu()]))
+    row_ind, col_ind, weight = stats.assign_clusters(y_pred, y_true)
+    acc = stats.cluster_acc(row_ind, col_ind, weight)
+    print(acc)
+    print(stats.cluster_confusion(row_ind, col_ind, weight))
+    return acc
+
+
+def cifar10_gcd(model, device="cpu"):
+    # get dataloader
+    batch_size = 128
+    norm_targets, novel_targets, (train_loader, _, _) = torch_cifar(
+            range(5), batch_size, include_novel=True, rot_loader=None)
+    # collect embeddings and labels
+    embeddings = np.empty((0, 768))
+    y_true = np.empty((0,))
+    for data, targets in train_loader:
+        data, targets = data.to(device), targets.to(device)
+        data_embeddings = model(data).detach().cpu().numpy()
+        embeddings = np.vstack((embeddings, data_embeddings))
+        y_true = np.hstack((y_true, targets.cpu().numpy()))
+    # SS KMeans
+    norm_mask = y_true < len(norm_targets)
+    ss_est = SSKMeans(embeddings[norm_mask], y_true[norm_mask], 10).fit(
+        embeddings[~norm_mask], y_true[~norm_mask])
+    y_pred = ss_est.predict(embeddings)
     row_ind, col_ind, weight = stats.assign_clusters(y_pred, y_true)
     acc = stats.cluster_acc(row_ind, col_ind, weight)
     print(acc)
