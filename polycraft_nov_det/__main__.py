0305227f21f3bb396bd66c13f403f608357309e9
PatrickFeeney <patrickfeeney1225@gmail.com>
2022-03-31 14:54:32 -0400

    Basic Arg Parsing

--- polycraft_nov_det/__main__.py
+++ polycraft_nov_det/__main__.py
@@ -1,12 +1,24 @@
+import argparse
+
 from polycraft_nov_det.data.cifar_loader import torch_cifar
 from polycraft_nov_det.models.autonovel_resnet import AutoNovelResNet
 from polycraft_nov_det.model_load import load_autonovel_resnet, load_dino_pretrained
 from polycraft_nov_det.models.dino_train import DinoWithHead
 import polycraft_nov_det.train as train
 
-mode = "gcd"
+# construct argument parser
+parser = argparse.ArgumentParser(description="Polycraft Novelty Detection Model Training")
+# add args
+parser.add_argument("-model", choices=["self_supervised", "supervised", "autonovel", "gcd"],
+                    default="gcd", help="Model to train")
+parser.add_argument("-gpu", type=int, default=1,
+                    help="Index of GPU to train on, negative int for CPU")
+args = parser.parse_args()
+# process args
+if args.gpu < 0:
+    args.gpu = None
 
-if mode == "self_supervised":
+if args.model == "self_supervised":
     # get dataloaders, note that batch is effectively 128 becuase of 4 rotations per image
     batch_size = 32
     _, _, (train_loader, _, _) = torch_cifar(
@@ -15,8 +27,8 @@ if mode == "self_supervised":
     model = AutoNovelResNet(4, 0)
     # start model training
     model_label = train.model_label(model, range(4))
-    train.train_self_supervised(model, model_label, train_loader, gpu=1)
-elif mode == "supervised":
+    train.train_self_supervised(model, model_label, train_loader, gpu=args.gpu)
+elif args.model == "supervised":
     # get dataloaders
     batch_size = 128
     norm_targets, _, (train_loader, _, _) = torch_cifar(range(5), batch_size)
@@ -26,8 +38,8 @@ elif mode == "supervised":
     model.freeze_layers()
     # start model training
     model_label = train.model_label(model, norm_targets)
-    train.train_supervised(model, model_label, train_loader, gpu=1)
-elif mode == "autonovel":
+    train.train_supervised(model, model_label, train_loader, gpu=args.gpu)
+elif args.model == "autonovel":
     # get dataloaders
     # TODO test this should be 64 and not 128 due to transform twice
     batch_size = 64
@@ -39,8 +51,8 @@ elif mode == "autonovel":
         strict=False, to_incremental=True)
     # start model training
     model_label = train.model_label(model, norm_targets)
-    train.train_autonovel(model, model_label, train_loader, norm_targets, gpu=1)
-elif mode == "gcd":
+    train.train_autonovel(model, model_label, train_loader, norm_targets, gpu=args.gpu)
+elif args.model == "gcd":
     batch_size = 128
     norm_targets, novel_targets, (train_loader, _, _) = torch_cifar(
         range(5), batch_size, include_novel=True, rot_loader="consistent")
@@ -48,4 +60,4 @@ elif mode == "gcd":
     model = DinoWithHead(load_dino_pretrained())
     # start model training
     model_label = train.model_label(model, norm_targets)
-    train.train_gcd(model, model_label, train_loader, norm_targets, gpu=1)
+    train.train_gcd(model, model_label, train_loader, norm_targets, gpu=args.gpu)
