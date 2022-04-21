a0b2da3523a30336f6554d40b92e4543a46ed665
Sarah Schneider <schneiders@C1SHP9901L>
2022-04-21 12:15:32 +0200

    VGG16 classifier training

--- polycraft_nov_det/train.py
+++ polycraft_nov_det/train.py
@@ -13,11 +13,9 @@ import polycraft_nov_det.plot as plot
 
 def model_label(model, include_classes):
     """Generate a label for the type of model being trained
-
     Args:
         model (torch.nn.Module): Model to generate label for.
         include_classes (iterable): Classes model will be trained on.
-
     Returns:
         str: Label for the model.
     """
@@ -32,7 +30,6 @@ def model_label(model, include_classes):
 
 def save_model(model, session_path, epoch):
     """Save a model.
-
     Args:
         model (torch.nn.Module): Model to save.
         session_path (pathlib.Path): Unique path segment for the training session from train.
@@ -49,7 +46,6 @@ def save_model(model, session_path, epoch):
 def train(model, model_label, train_loader, valid_loader, lr, epochs=500, train_noisy=True,
           gpu=None):
     """Train a model.
-
     Args:
         model (torch.nn.Module): Model to train.
         model_label (str): Label for model type, preferably from model_label function.
@@ -59,7 +55,6 @@ def train(model, model_label, train_loader, valid_loader, lr, epochs=500, train_
         epochs (int, optional): Number of epochs to train for. Defaults to 500.
         train_noisy (bool, optional): Whether to use denoising autoencoder. Defaults to True.
         gpu (int, optional): Index of GPU to use, CPU if None. Defaults to None.
-
     Returns:
         torch.nn.Module: Trained model.
     """
@@ -109,6 +104,67 @@ def train(model, model_label, train_loader, valid_loader, lr, epochs=500, train_
         if (epochs >= 10 and (epoch + 1) % (epochs // 10) == 0) or epoch == epochs - 1:
             # get reconstruction visualization
             writer.add_figure("Reconstruction Vis", plot.plot_reconstruction(data, r_data), epoch)
+            # save model
+            save_model(model, session_path, epoch)
+    return model
+
+def train_VGG(model, train_loader, valid_loader, lr, epochs=500, train_noisy=True, gpu=None):
+    """Train a VGG model.
+    Args:
+        model (torch.nn.Module): (Pretrained) VGG Classifiction Model to train.
+        train_loader (torch.utils.data.DataLoader): Training set for model.
+        valid_loader (torch.utils.data.DataLoader): Validation set for model.
+        lr (float): Learning rate.
+        epochs (int, optional): Number of epochs to train for. Defaults to 500.
+        train_noisy (bool, optional): Whether to use denoising autoencoder. Defaults to True.
+        gpu (int, optional): Index of GPU to use, CPU if None. Defaults to None.
+    Returns:
+        torch.nn.Module: Trained VGG model.
+    """
+    # get a unique path for this session to prevent overwriting
+    start_time = datetime.now().strftime("%Y.%m.%d.%H.%M.%S")
+    session_path = pathlib.Path(model_label) / pathlib.Path(start_time)
+    # get Tensorboard writer
+    writer = SummaryWriter(pathlib.Path("runs_VGG") / session_path)
+    # define training constants
+    loss_func = nn.CrossEntropyLoss()
+    device = torch.device(gpu if gpu is not None else "cpu")
+    # move model to device
+    model.to(device)
+    # construct optimizer
+    optimizer = optim.Adam(model.parameters(), lr)
+    # train model
+    for epoch in range(epochs):
+        train_loss = 0
+        for data, target in train_loader:
+            batch_size = data.shape[0]
+            data = data.to(device)
+            optimizer.zero_grad()
+            # update weights with optimizer
+            if not train_noisy:
+                pred = model(data)
+            else:
+                pred = model(GaussianNoise()(data))
+            batch_loss = loss_func(target, pred)
+            batch_loss.backward()
+            optimizer.step()
+            # logging
+            train_loss += batch_loss.item() * batch_size
+        # calculate and record train loss
+        av_train_loss = train_loss / len(train_loader)
+        writer.add_scalar("Average Train Loss", av_train_loss, epoch)
+        # get validation loss
+        valid_loss = 0
+        for data, target in valid_loader:
+            batch_size = data.shape[0]
+            data = data.to(device)
+            pred = model(data)
+            batch_loss = loss_func(target, pred)
+            valid_loss += batch_loss.item() * batch_size
+        av_valid_loss = valid_loss / len(valid_loader)
+        writer.add_scalar("Average Validation Loss", av_valid_loss, epoch)
+        # updates every 10% of training time
+        if (epochs >= 10 and (epoch + 1) % (epochs // 10) == 0) or epoch == epochs - 1:
             # save model
             save_model(model, session_path, epoch)
     return model
