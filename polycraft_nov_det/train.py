a95bc61397128c5d89d754c93b62d4b96048da88
PatrickFeeney <patrickfeeney1225@gmail.com>
2022-04-27 10:44:26 -0400

    Code Formatting

--- polycraft_nov_det/train.py
+++ polycraft_nov_det/train.py
@@ -1,14 +1,10 @@
 from datetime import datetime
 import pathlib
 
-import matplotlib.pyplot as plt
-import numpy as np
-
 import torch
 import torch.nn as nn
 import torch.optim as optim
 from torch.utils.tensorboard import SummaryWriter
-import torch.nn.functional as F
 
 from polycraft_nov_data.image_transforms import GaussianNoise
 from polycraft_nov_data.dataloader import polycraft_dataloaders_full_image
@@ -116,7 +112,7 @@ def train(model, model_label, train_loader, valid_loader, lr, epochs=500, train_
     return model
 
 
-def train_VGG(model, train_loader, valid_loader, lr, epochs=500, train_noisy=True, gpu=None):
+def train_vgg(model, train_loader, valid_loader, lr, epochs=500, train_noisy=True, gpu=None):
     """Train a VGG model.
     Args:
         model (torch.nn.Module): (Pretrained) VGG Classifiction Model to train.
@@ -142,107 +138,73 @@ def train_VGG(model, train_loader, valid_loader, lr, epochs=500, train_noisy=Tru
     model.to(device)
     # construct optimizer
     optimizer = optim.Adam(model.parameters(), lr)
-    
-    normal_class_indices = torch.tensor([0, 1, 11, 36, 53]) 
-    
+    normal_class_indices = torch.tensor([0, 1, 11, 36, 53])
     # train model
     for epoch in range(epochs):
         print('---------------------------------------------', flush=True)
         print('Epoch Nr.', epoch, flush=True)
         train_loss = 0
-        
         for data, target in train_loader:
             batch_size = data.shape[0]
             target_ = torch.empty_like(target)
-            
             for data_idx in range(batch_size):
                 for nci_idx, nci in enumerate(normal_class_indices):
-                
                     if target[data_idx] == nci:
                         target_[data_idx] = torch.tensor(nci_idx, dtype=torch.long)
-                        
-                #plt.imshow(np.transpose(data[data_idx].detach().cpu(), (1, 2, 0)))
-                #plt.title(str(target_[data_idx]))
-                #plt.savefig('sanity_check/' + str(data_idx) + '.png') 
-                              
             data = data.to(device)
-            
             target_ = target_.to(device)
-            
             optimizer.zero_grad()
-            
             # update weights with optimizer
             if not train_noisy:
                 pred = model(data)
             else:
                 pred = model(GaussianNoise()(data))
-            
             batch_loss = loss_func(pred, target_)
             batch_loss.backward()
             optimizer.step()
             # logging
             train_loss += batch_loss.item() * batch_size
-            
-       
         # calculate and record train loss
         av_train_loss = train_loss / len(train_loader)
-        print('Avg. Train loss ', av_train_loss, flush = True)
+        print('Avg. Train loss ', av_train_loss, flush=True)
         writer.add_scalar("Average Train Loss", av_train_loss, epoch)
-        
         # get validation loss
         valid_loss = 0
         for data, target in valid_loader:
-            
             batch_size = 0
             batch_size_max = data.shape[0]
             target_ = []
             data_ = []
-            
             for data_idx in range(batch_size_max):
                 for nci_idx, nci in enumerate(normal_class_indices):
-                
                     # use only seen novelties for classifier validation
                     if target[data_idx] == nci:
                         target_.append(torch.tensor(nci_idx, dtype=torch.long))
                         data_.append(data[data_idx])
                         batch_size += 1
-                        
-            #for data_idx in range(batch_size):           
-            #    plt.imshow(np.transpose(data_[data_idx].detach().cpu(), (1, 2, 0)))
-            #    plt.title(str(target_[data_idx]))
-            #    plt.savefig('sanity_check/' + str(data_idx) + '.png')
-                        
             data_ = torch.stack(data_)
             target_ = torch.stack(target_)
-            
             data_ = data_.to(device)
             target_ = target_.to(device)
             pred = model(data_)
             batch_loss = loss_func(pred, target_)
             valid_loss += batch_loss.item() * batch_size
-            
         av_valid_loss = valid_loss / len(valid_loader)
-        print('Avg. Valid loss ', av_valid_loss, flush = True)
+        print('Avg. Valid loss ', av_valid_loss, flush=True)
         writer.add_scalar("Average Validation Loss", av_valid_loss, epoch)
         # updates every 10% of training time
         if (epochs >= 10 and (epoch + 1) % (epochs // 10) == 0) or epoch == epochs - 1:
             # save model
             save_model(model, session_path, epoch)
-       
     return model
 
 
 if __name__ == '__main__':
-    
-    
-    train_loader, valid_loader, _ = polycraft_dataloaders_full_image(batch_size=32, 
-                                                          image_scale=1, 
-                                                          include_novel=True, 
-                                                          shuffle=True)
-    
+    train_loader, valid_loader, _ = polycraft_dataloaders_full_image(batch_size=32,
+                                                                     image_scale=1,
+                                                                     include_novel=True,
+                                                                     shuffle=True)
     print('Loading is done', flush=True)
-    
     classifier = vgg16.VGGPretrained(num_classes=5)
-    
-    train_VGG(classifier, train_loader, valid_loader, lr=1e-5, epochs=1000, 
+    train_vgg(classifier, train_loader, valid_loader, lr=1e-5, epochs=1000,
               train_noisy=True, gpu=1)
