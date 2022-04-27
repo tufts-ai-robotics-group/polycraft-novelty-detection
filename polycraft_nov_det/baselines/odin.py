74842389dc47ec312655c6c77178ebe1401b119b
PatrickFeeney <patrickfeeney1225@gmail.com>
2022-04-27 14:30:54 -0400

    ODIN Implementation

---
+++ polycraft_nov_det/baselines/odin.py
@@ -0,0 +1,32 @@
+import torch
+from torchvision.transforms import Normalize
+
+from polycraft_nov_det.detector import NoveltyDetector
+
+
+class OdinDetector(NoveltyDetector):
+    def __init__(self, model, temp=1, noise=.0014):
+        super().__init__()
+        self.model = model.eval()
+        self.temp = temp
+        self.noise = noise
+
+    def novelty_score(self, data):
+        # get model output
+        output = self.model(data)
+        # get prediction from non-temp scaled output
+        pred = torch.argmax(torch.softmax(output, dim=1), dim=1)
+        # perturbation from backprop temp scaled CE
+        loss = torch.nn.CrossEntropyLoss()(output / self.temp, pred)
+        loss.backward()
+        # normalizing the gradient to binary in {0, 1}
+        gradient = torch.ge(data.grad.data, 0)
+        gradient = (gradient.float() - 0.5) * 2
+        # normalizing the gradient to the same space of image
+        gradient = Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])(gradient)
+        # adding small perturbations to images
+        pert_data = torch.add(data.data, -self.noise, gradient)
+        output = self.model(pert_data)
+        # score from perturbed image
+        novel_score = torch.max(torch.softmax(output / self.temp, dim=1), dim=1)
+        return novel_score
