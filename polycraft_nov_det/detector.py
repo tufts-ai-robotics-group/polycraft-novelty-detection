9220631dbb71ba5e4c31f9201885a1d18b351653
PatrickFeeney <patrickfeeney1225@gmail.com>
2022-05-12 12:09:26 -0400

    Fix Shape of Null Detector

--- polycraft_nov_det/detector.py
+++ polycraft_nov_det/detector.py
@@ -164,7 +164,7 @@ class NoveltyDetector:
 
     @torch.no_grad()
     def novelty_score(self, data):
-        return 0
+        return torch.zeros(data.shape[0])
 
     def is_novel(self, data, thresh):
         return self.novelty_score(data) > thresh
