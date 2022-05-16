6e9e5221b22e51067e1bd40116a392f11895cce8
PatrickFeeney <patrickfeeney1225@gmail.com>
2022-05-16 15:03:06 -0400

    Ensemble Models

--- polycraft_nov_det/baselines/ensemble.py
+++ polycraft_nov_det/baselines/ensemble.py
@@ -17,3 +17,23 @@ class EnsembleDetector(NoveltyDetector):
         outputs = torch.stack(outputs)
         # selecting first element is only to discard argmax from torch.max
         return -1 * torch.max(torch.mean(outputs, dim=0), dim=-1)[0]
+
+
+if __name__ == "__main__":
+    from pathlib import Path
+
+    from polycraft_nov_det.baselines.eval_polycraft import save_scores, eval_from_save
+    from polycraft_nov_det.model_utils import load_vgg_model
+
+    device = torch.device("cuda:1")
+    models = [
+        load_vgg_model(Path("models/vgg/vgg_classifier_1000.pt", device)),
+        load_vgg_model(Path("models/vgg/vgg_classifier_1000_2.pt", device)),
+        load_vgg_model(Path("models/vgg/vgg_classifier_1000_3.pt", device)),
+        load_vgg_model(Path("models/vgg/vgg_classifier_1000_4.pt", device)),
+        load_vgg_model(Path("models/vgg/vgg_classifier_1000_5.pt", device)),
+    ]
+    output_folder = Path("models/vgg/eval_ensemble/")
+
+    save_scores(EnsembleDetector(models, device), output_folder)
+    eval_from_save(output_folder)
--- scripts/download_models.py
+++ scripts/download_models.py
@@ -3,11 +3,23 @@ import urllib.request
 
 
 model_path_to_url = {
+    Path("models/vgg/ndcc_stanford_dogs_30.pt"):
+    "https://drive.google.com/uc?export=download&id=1gm8GvXkTjgw-1u3YBdqioOGG0onD9si9&confirm=t",
+
     Path("models/vgg/vgg_classifier_1000.pt"):
     "https://drive.google.com/uc?export=download&id=1bw5pYUPKPg6WlMyowuzG-nfCTBeJvvS0&confirm=t",
 
-    Path("models/vgg/ndcc_stanford_dogs_30.pt"):
-    "https://drive.google.com/uc?export=download&id=1gm8GvXkTjgw-1u3YBdqioOGG0onD9si9&confirm=t",
+    Path("models/vgg/vgg_classifier_1000_2.pt"):
+    "https://drive.google.com/uc?export=download&id=1RZG8ZqgYoq5ihbVFq0yaYQjG9qNCGnO_&confirm=t",
+
+    Path("models/vgg/vgg_classifier_1000_3.pt"):
+    "https://drive.google.com/uc?export=download&id=1R4gwJsPJRtkDILjpa06tmpkBYs5z-cwD&confirm=t",
+
+    Path("models/vgg/vgg_classifier_1000_4.pt"):
+    "https://drive.google.com/uc?export=download&id=1MtPn-D0AP7bgpObT0IKiQoS06YpZcas1&confirm=t",
+
+    Path("models/vgg/vgg_classifier_1000_5.pt"):
+    "https://drive.google.com/uc?export=download&id=1HNjKNYXTkFqNR-O6HWAoAIIXxjNbUMtK&confirm=t",
 }
 
 if __name__ == "__main__":
