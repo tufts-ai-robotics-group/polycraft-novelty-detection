81ed0bb3aed0772574fa15a0f8008cc395245969
Schneider Sarah <Sarah.Schneider@ait.ac.at>
2022-05-23 09:24:56 +0200

    grid search with recall/precision

--- polycraft_nov_det/baselines/oneclassSVM.py
+++ polycraft_nov_det/baselines/oneclassSVM.py
@@ -6,10 +6,13 @@ import torch
 import matplotlib.pyplot as plt
 
 from sklearn.svm import OneClassSVM
+from sklearn.preprocessing import MinMaxScaler
+from pathlib import Path
 
 from polycraft_nov_data.dataloader import polycraft_dataloaders
-import polycraft_nov_det.model_utils as model_utils
+
 from polycraft_nov_det.detector import NoveltyDetector
+from polycraft_nov_det.model_utils import load_vgg_model
 
 
 def extract_features(feature_extractor, loader):
@@ -26,11 +29,14 @@ def extract_features(feature_extractor, loader):
     features_list = []
     target_list = []
     print('Lets collect features')
+    
+    normalizer = MinMaxScaler()
 
     for j, (data, target) in enumerate(loader):
         # novel --> 1, not novel (class 0, 1, 2, 3, 4) --> 0
         target = [0 if (i == 0 or i == 1 or i ==2 or i == 3 or i == 4) else (1) for i in target]
         target_list.append(target)
+        print(data.shape)
         features = feature_extractor(data).detach().numpy()
         features_list.append(features)
 
@@ -40,7 +46,9 @@ def extract_features(feature_extractor, loader):
     print('Features collected')
     features_list = np.concatenate(features_list)
     target_list = np.concatenate(target_list)
-
+    print(features_list.shape)
+    features_list = normalizer.fit_transform(features_list)
+    
     return features_list, target_list
 
 
@@ -50,10 +58,11 @@ class OneClassSVMDetector(NoveltyDetector):
         self.svm = svm
         
     def novelty_score(self, features):
-        outputs = self.svm.predict(features)
-        # svm output +1 --> not novel (0), svm output -1 --> novel
+        # svm outputs are positive for inliers, negative for outliers
+        outputs = self.svm.decision_function(features)
         outputs = torch.Tensor(outputs)
-        outputs = [0 if (i == +1) else 1 for i in outputs]
+        
+        outputs = [0 if (i >= 0) else 1 for i in outputs]
         return outputs
 
 
@@ -77,15 +86,16 @@ def fit_oneclassSVM(feature_extractor, train_loader, valid_loader):
 
     nu_range = np.linspace(0.1, 0.9, 9)
     gamma_range = np.logspace(-5, 5, 11)
-    acc_nn = np.zeros((len(nu_range), len(gamma_range)))
-    acc_n = np.zeros((len(nu_range), len(gamma_range)))
+    
+    precision = np.zeros((len(nu_range), len(gamma_range)))
+    recall = np.zeros((len(nu_range), len(gamma_range)))
 
     for i, nu in enumerate(nu_range):
         for j, gamma in enumerate(gamma_range):
             print('--------------------------', flush = True)
             print('Nu ', nu)
             print('Gamma ', gamma)
-
+            
             # OneClassSVM based on Schölkopf et. al.
             svm = OneClassSVM(nu=nu, kernel='rbf', gamma=gamma)
 
@@ -93,35 +103,56 @@ def fit_oneclassSVM(feature_extractor, train_loader, valid_loader):
             oc_svm = svm.fit(features_train)
             detector = OneClassSVMDetector(oc_svm)
             svm_pred = detector.novelty_score(features_train) 
+            
             # calculate accuracy
             train_acc = np.sum(svm_pred == targets_train)/len(features_train)
             print('Train Acc.: ', train_acc)
 
             # apply svm (fitted on training data) to validation data (with novelties)
             svm_pred = detector.novelty_score(features_valid) 
-
-            # calculate accuracy
             valid_acc = np.sum(svm_pred == targets_valid)/len(features_valid)
             print('Valid Acc.: ', valid_acc)
 
-            acc_per_class = []
+            corr_per_class = []
+            num_per_class = []
+            
+            svm_pred = np.asarray([bool(x) for x in svm_pred])
+            targets_valid = np.asarray([bool(x) for x in targets_valid])
+            
 
+            t_pos = np.sum(np.logical_and(svm_pred, targets_valid))
+            f_pos = np.sum(np.logical_and(svm_pred, ~targets_valid))
+            t_neg = np.sum(np.logical_and(~svm_pred, ~targets_valid))
+            f_neg = np.sum(np.logical_and(~svm_pred, targets_valid))
+            
+            print('t_pos', t_pos)
+            print('t_neg', t_neg)
+            print('f_pos', f_pos)
+            print('f_neg', f_neg)
+            
             for c in range(2):  # we have only non-novel and novel class
                 match = targets_valid == c
                 noi_per_class = match.sum()
+                num_per_class.append(noi_per_class)
+                
                 match = match & (svm_pred == targets_valid)
                 num_corrects_per_class = match.sum()
-                acc_per_class.append(num_corrects_per_class / noi_per_class)
-
-            print('Valid Acc, class non-novel: ', acc_per_class[0], flush=True)
-            print('Valid Acc, class novel: ', acc_per_class[1], flush=True)
+                corr_per_class.append(num_corrects_per_class)
+                
+                
+            print('tp', corr_per_class[1])
+            print('tn', corr_per_class[0])
             
-            acc_nn[i][j] = acc_per_class[0]
-            acc_n[i][j] = acc_per_class[1]
-
+            print('Valid Acc, class non-novel: ', corr_per_class[0]/num_per_class[0], flush=True)
+            print('Valid Acc, class novel: ', corr_per_class[1]/num_per_class[0], flush=True)
+            
+            precision[i][j] = t_pos/(t_pos + f_pos)
+            recall[i][j] = t_pos/(t_pos + f_neg)
+            print("nu_%f_gamm_%f.pkl" % (nu, gamma))
+           
             # construct paths
             model_dir = pathlib.Path("models") / session_path
-            model_fname = pathlib.Path("nu_%d_gamm_%d.pkl" % (nu, gamma))
+            model_fname = pathlib.Path("nu_%f_gamm_%f.pkl" % (nu, gamma))
             # make directory and save model
             model_dir.mkdir(parents=True, exist_ok=True)
             joblib.dump(oc_svm, model_dir / model_fname)
@@ -129,18 +160,18 @@ def fit_oneclassSVM(feature_extractor, train_loader, valid_loader):
     X, Y = np.meshgrid(gamma_range, nu_range)
             
     fig, ax = plt.subplots(1, 2, constrained_layout=True, figsize=(12,5), dpi=300)
-    p = ax[0].pcolor(X, Y, acc_nn, vmin=abs(acc_nn).min(), vmax=abs(acc_nn).max())
+    p = ax[0].pcolor(X, Y, precision, vmin=abs(precision).min(), vmax=abs(precision).max())
     ax[0].set_xscale('log')
     ax[0].set_xlabel('gamma')
     ax[0].set_ylabel('nu')
-    ax[0].title.set_text('Non-novel accuracy')
+    ax[0].title.set_text('Precision')
     cb = fig.colorbar(p, ax=ax[0])
     
-    p = ax[1].pcolor(X, Y, acc_n, vmin=abs(acc_n).min(), vmax=abs(acc_n).max())
+    p = ax[1].pcolor(X, Y, recall, vmin=abs(recall).min(), vmax=abs(recall).max())
     ax[1].set_xscale('log')
     ax[1].set_xlabel('gamma')
     ax[1].set_ylabel('nu')
-    ax[1].title.set_text('Novel accuracy')
+    ax[1].title.set_text('Recall')
     cb = fig.colorbar(p, ax=ax[1])
     
     fig.savefig('param_search.png')
@@ -150,7 +181,7 @@ def fit_oneclassSVM(feature_extractor, train_loader, valid_loader):
 
 if __name__ == '__main__':
 
-    gpu = 1
+    gpu = 0
     num_classes = 5
     device = torch.device(gpu if gpu is not None else "cpu")
 
@@ -161,8 +192,7 @@ if __name__ == '__main__':
                                                           shuffle=True, 
                                                           ret_class_to_idx=True)
     
-    cl_path = '../models/VGGPretrained_class_normal_fence_item_anvil_item_sand_item_coal_block/2022.05.10.11.51.34/1000.pt'
-    cl_path = pathlib.Path(cl_path)
-    classifier = model_utils.load_polycraft_classifier(cl_path, device, num_classes=num_classes).eval()
-    svm = fit_oneclassSVM(classifier, train_loader, valid_loader)
+    
+    classifier = load_vgg_model(Path("../models/VGGPretrained_class_normal_fence_item_anvil_item_sand_item_coal_block/2022.05.10.11.51.34/1000.pt"), device)
+    svm = fit_oneclassSVM(classifier.eval(), train_loader, valid_loader)
     
