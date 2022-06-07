9730b7f0524358b80e1e08e29ed32098c49bc692
Sarah Schneider <schneiders@C1SHP9901L>
2022-06-07 06:15:29 +0200

    new score files

--- polycraft_nov_det/baselines/eval_polycraft.py
+++ polycraft_nov_det/baselines/eval_polycraft.py
@@ -86,9 +86,9 @@ def eval_from_save(output_folder):
 if __name__ == "__main__":
     method_to_outputs = {
         "NDCC": Path("models/vgg/eval_ndcc/stanford_dogs_times_1e-1"),
-        "ODIN": Path("models/vgg/eval_odin/t=1000_n=0.0004"),
+        "ODIN": Path("models/vgg/eval_odin/t=1000_n=0.0000"),
         "Ensemble": Path("models/vgg/eval_ensemble/"),
-        "One-Class SVM": Path("models/vgg/eval_ocsvm/nu=0.100000_gamm=0.000010"),
+        "One-Class SVM": Path("models/vgg/eval_ocsvm/nu=0.800000_gamm=0.000010"),
         "Autoencoder (Patch)": Path("models/polycraft/noisy/scale_1/patch_based/AE_patchwise"),
         "Autoencoder (Full image)": Path("models/polycraft/noisy/scale_1/fullimage_based/AE_fullimage")}
     for method, output_folder in method_to_outputs.items():
