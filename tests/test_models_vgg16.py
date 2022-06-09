79330bf339aefbdd4f5e29137e83259fb30dcc3b
PatrickFeeney <patrickfeeney1225@gmail.com>
2022-06-09 12:30:31 -0400

    Fix VGG Test Allocation Error

--- tests/test_models_vgg16.py
+++ tests/test_models_vgg16.py
@@ -1,16 +1,12 @@
-import pytest
 import torch
 
 import polycraft_nov_det.models.vgg as vgg
 
 
-batch_size = 64
+batch_size = 8
 
 
 class TestVGG():
-    @pytest.fixture
-    def model(self):
-        return vgg.VGGPretrained(3)
-
-    def test_forward(self, model):
+    def test_forward(self):
+        model = vgg.VGGPretrained(3)
         model(torch.zeros((batch_size,) + (3, 256, 256 - 22)))
