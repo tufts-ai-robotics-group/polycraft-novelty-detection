d6d002616ff82b7a1ec0371b4a3a17169d03eec4
PatrickFeeney <patrickfeeney1225@gmail.com>
2022-07-15 15:59:25 -0400

    Remove Deprecated Eval

--- polycraft_nov_det/eval/default.py
+++
@@ -1,9 +0,0 @@
-from polycraft_nov_det.eval.eval import eval_mnist, eval_polycraft
-
-
-def def_eval_mnist():
-    eval_mnist("./models/mnist/class_0-4/noisy_500_lr_1e-2/500.pt")
-
-
-def def_eval_polycraft():
-    eval_polycraft("./models/polycraft/noisy/scale_0_75/8000.pt", image_scale=.75, device="cuda:1")
