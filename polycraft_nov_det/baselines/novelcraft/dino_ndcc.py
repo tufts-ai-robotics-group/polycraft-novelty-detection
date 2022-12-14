3201a51ec976d8f91165e07612792d273835de67
PatrickFeeney <patrickfeeney1225@gmail.com>
2022-12-14 10:50:52 -0500

    DINONDCCDetector

--- polycraft_nov_det/baselines/novelcraft/dino_ndcc.py
+++ polycraft_nov_det/baselines/novelcraft/dino_ndcc.py
@@ -1,11 +1,32 @@
 from pathlib import Path
 
+import numpy as np
 import torch
 
 from polycraft_nov_data.dataloader import novelcraft_dataloader
 from polycraft_nov_data.image_transforms import VGGPreprocess
 
-from polycraft_nov_det.baselines.novelcraft.ndcc import NDCCDetector
+from polycraft_nov_det.baselines.novelcraft.ndcc import mahalanobis_metric
+from polycraft_nov_det.detector import NoveltyDetector
+
+
+class DINONDCCDetector(NoveltyDetector):
+    def __init__(self, model, device="cpu"):
+        super().__init__(device)
+        self.model = model.eval().to(device)
+        # init Gaussian params
+        means, sigma2s = model.gaussian_params()
+        self.means = means.cpu().detach().numpy()
+        self.inv_sigma = np.diag(sigma2s.detach().cpu().numpy() ** -1)
+
+    def novelty_score(self, data):
+        data = data.to(self.device)
+        with torch.no_grad():
+            outputs = self.model(data)
+            outputs = outputs.detach().cpu().numpy().squeeze()
+        distances = mahalanobis_metric(outputs, self.means, self.inv_sigma)
+        nd_scores = np.min(distances, axis=1)
+        return torch.Tensor(nd_scores)
 
 
 if __name__ == "__main__":
@@ -16,5 +37,5 @@ if __name__ == "__main__":
     device = torch.device("cuda:1")
     model = torch.hub.load("tufts-ai-robotics-group/CCGaussian", "dino_ccg")
     output_folder = Path("models/dino_ndcc/")
-    save_scores(NDCCDetector(model, device), output_folder, valid_loader, test_loader)
+    save_scores(DINONDCCDetector(model, device), output_folder, valid_loader, test_loader)
     eval_from_save(output_folder)
