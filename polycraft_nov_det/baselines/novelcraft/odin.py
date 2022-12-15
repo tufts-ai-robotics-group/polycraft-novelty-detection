b1b53d461264bc1f61b98f39a03864df00d69f70
Schneider Sarah <Sarah.Schneider@ait.ac.at>
2022-12-15 16:20:05 +0100

    odin+ eval stat files

--- polycraft_nov_det/baselines/novelcraft/odin.py
+++ polycraft_nov_det/baselines/novelcraft/odin.py
@@ -55,7 +55,7 @@ if __name__ == "__main__":
         model_path = Path("models/vgg/vgg_classifier_1000.pt")
         
     # define hyperparameter search space
-    temps = [1, 2, 5, 10, 20, 50, 100, 200, 500, 1000]
+    temps = [1000]
     noises = [i * 0.004 / 20 for i in range(21)]
     for temp in temps:
         for noise in noises:
