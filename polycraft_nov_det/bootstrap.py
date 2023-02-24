6b2e3694464c798b51297cba34c868947b40e42d
PatrickFeeney <patrickfeeney1225@gmail.com>
2023-02-24 11:57:48 -0500

    Bootstrap Code Outline

---
+++ polycraft_nov_det/bootstrap.py
@@ -0,0 +1,19 @@
+
+
+def bootstrap_metric(y_pred, y_true, metric_func):
+    """Compute test set boostrapping of a metric
+
+    Args:
+        y_pred (torch.Tensor): Model predictions for some output y
+        y_true (torch.Tensor): True value of output y
+        metric_func (function): function with parameters (y_pred, y_true) returning a float metric
+
+    Returns:
+        tuple: metric_mean: float with bootstrapped mean of metric
+               ci_low: Low value from 95% confidence interval
+               ci_high: High value from 95% confidence interval
+    """
+    metric_mean = 0.
+    ci_low = 0.
+    ci_high = 0.
+    return (metric_mean, ci_low, ci_high)
---
+++ tests/test_bootstrap.py
@@ -0,0 +1,5 @@
+from polycraft_nov_det.bootstrap import bootstrap_metric
+
+
+def test_bootstrap():
+    return True
